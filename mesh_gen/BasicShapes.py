6e497497aab9783f1d3d9aceb42e00fb00c93925
Paul Nguyen <pn2yr@virginia.edu>
2017-05-27 22:53:19 -0500

    Fixed torus bug

--- mesh_gen/BasicShapes.py
+++ mesh_gen/BasicShapes.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 
-from Mesh import Mesh
+from Mesh import *
 import random
 import pdb
 import time
@@ -47,6 +47,7 @@ def cube(length=1):
     return m
 
 def cone(height=10, radius=5, num_triangles=360):
+    # num_triangles is the number of triangles used for the part of the cone that isn't the base 
     m=Mesh()
     for triangle_index in range(num_triangles):
         start_angle = 2*pi/float(num_triangles)*triangle_index
@@ -62,10 +63,42 @@ def cone(height=10, radius=5, num_triangles=360):
     for triangle_index in range(num_triangles):
         start_angle = 2*pi/float(num_triangles)*triangle_index
     return m
-                    
+
+def torus(inner_radius=5, outer_radius=10, num_segments=36, segment_precision=36):
+    # num_segments refers to the number of segments we split the donut/torus into (we cut from the center outward)
+    # segment_precision refers to the number of rectangles used per segment 
+    m=Mesh()
+    assert inner_radius < outer_radius
+    tube_radius = (outer_radius-inner_radius)/2.0
+    for segment_index in range(num_segments): # index along the length of the tube (the long part if we're thinking about a regular donut)
+        lengthwise_start_angle = 2*pi/float(num_segments)*segment_index
+        lengthwise_end_angle = 2*pi/float(num_segments)*(segment_index+1)
+        lengthwise_tube_start_center_x = (inner_radius+tube_radius)*cos(lengthwise_start_angle)
+        lengthwise_tube_start_center_y = (inner_radius+tube_radius)*sin(lengthwise_start_angle)
+        lengthwise_tube_start_center_z = 0
+        lengthwise_tube_end_center_x = (inner_radius+tube_radius)*cos(lengthwise_end_angle)
+        lengthwise_tube_end_center_y = (inner_radius+tube_radius)*sin(lengthwise_end_angle)
+        lengthwise_tube_end_center_z = 0
+        for rect_index in range(segment_precision): # index along the tube's circumference
+            slicewise_tube_start_angle = 2*pi/float(segment_precision)*rect_index
+            slicewise_tube_end_angle = 2*pi/float(segment_precision)*(rect_index+1)
+            # innertube coordinates
+            start_circle_coords = rotate_about_z_axis(lengthwise_start_angle, tube_radius*cos(slicewise_tube_start_angle),0,tube_radius*sin(slicewise_tube_start_angle))
+            start_circle_coords_further_along_slice = rotate_about_z_axis(lengthwise_start_angle, tube_radius*cos(slicewise_tube_end_angle),0,tube_radius*sin(slicewise_tube_end_angle))
+            end_circle_coords = rotate_about_z_axis(lengthwise_end_angle, tube_radius*cos(slicewise_tube_start_angle),0,tube_radius*sin(slicewise_tube_start_angle))
+            end_circle_coords_further_along_slice = rotate_about_z_axis(lengthwise_end_angle, tube_radius*cos(slicewise_tube_end_angle),0,tube_radius*sin(slicewise_tube_end_angle))
+            m.add_face([
+                        (lengthwise_tube_end_center_x+end_circle_coords[X_COORD],lengthwise_tube_end_center_y+end_circle_coords[Y_COORD],lengthwise_tube_end_center_z+end_circle_coords[Z_COORD]),
+                        (lengthwise_tube_end_center_x+end_circle_coords_further_along_slice[X_COORD],lengthwise_tube_end_center_y+end_circle_coords_further_along_slice[Y_COORD],lengthwise_tube_end_center_z+end_circle_coords_further_along_slice[Z_COORD]),
+                        (lengthwise_tube_start_center_x+start_circle_coords_further_along_slice[X_COORD],lengthwise_tube_start_center_y+start_circle_coords_further_along_slice[Y_COORD],lengthwise_tube_start_center_z+start_circle_coords_further_along_slice[Z_COORD]),
+                        (lengthwise_tube_start_center_x+start_circle_coords[X_COORD],lengthwise_tube_start_center_y+start_circle_coords[Y_COORD],lengthwise_tube_start_center_z+start_circle_coords[Z_COORD]),
+                        ])
+    return m
+
 def main():
     #m=cube()
-    m=cone()
+    #m=cone()
+    m=torus()
     m.save_to_obj_file("C:/Users/nguye/Desktop/out.obj")
 
 if __name__ == '__main__':
--- mesh_gen/Mesh.py
+++ mesh_gen/Mesh.py
@@ -3,6 +3,59 @@
 import random
 import pdb
 import time
+from math import cos, sin, pi
+
+X_COORD=0
+Y_COORD=1
+Z_COORD=2
+
+def rotate_about_x_axis(angle_radians, *args):
+    if len(args)==1:
+        x0 = args[0][X_COORD]
+        y0 = args[0][Y_COORD]
+        z0 = args[0][Z_COORD]
+    elif len(args)==3:
+        x0 = args[X_COORD]
+        y0 = args[Y_COORD]
+        z0 = args[Z_COORD]
+    else: 
+        assert False
+    x = x0 
+    y = cos(angle_radians)*y0-sin(angle_radians)*z0
+    z = sin(angle_radians)*y0+cos(angle_radians)*z0
+    return (x,y,z)
+
+def rotate_about_y_axis(angle_radians, *args):
+    if len(args)==1:
+        x0 = args[0][X_COORD]
+        y0 = args[0][Y_COORD]
+        z0 = args[0][Z_COORD]
+    elif len(args)==3:
+        x0 = args[X_COORD]
+        y0 = args[Y_COORD]
+        z0 = args[Z_COORD]
+    else: 
+        assert False
+    x = cos(angle_radians)*x0+sin(angle_radians)*z0 
+    y = y0
+    z = -sin(angle_radians)*x0+cos(angle_radians)*z0
+    return (x,y,z)
+
+def rotate_about_z_axis(angle_radians, *args):
+    if len(args)==1:
+        x0 = args[0][X_COORD]
+        y0 = args[0][Y_COORD]
+        z0 = args[0][Z_COORD]
+    elif len(args)==3:
+        x0 = args[X_COORD]
+        y0 = args[Y_COORD]
+        z0 = args[Z_COORD]
+    else: 
+        assert False
+    x = cos(angle_radians)*x0-sin(angle_radians)*y0
+    y = sin(angle_radians)*x0+cos(angle_radians)*y0
+    z = z0
+    return (x,y,z)
 
 class Mesh(object): 
     
@@ -11,14 +64,24 @@ class Mesh(object):
         self.face_list = [] # faces are lists. These lists contain vertex indices.
         self.vertex_count=0
     
-    def add_vertex(self, x,y,z):
+    def add_vertex(self, *args):
+        if len(args)==1:
+            x = round(args[0][X_COORD],10)
+            y = round(args[0][Y_COORD],10)
+            z = round(args[0][Z_COORD],10)
+        elif len(args)==3:
+            x = round(args[X_COORD],10)
+            y = round(args[Y_COORD],10)
+            z = round(args[Z_COORD],10)
+        else: 
+            assert False
         if (x,y,z) not in self.vertex_dict:
             self.vertex_dict[(x,y,z)]= self.vertex_count+1 
             self.vertex_count += 1
+        return (x,y,z)
     
     def add_face(self, v_list):
-        for v in v_list:
-            self.add_vertex(v[0],v[1],v[2])
+        v_list = map(self.add_vertex, v_list)
         self.face_list.append([self.vertex_dict[v] for v in v_list])
     
     def save_to_obj_file(self, output_file):
@@ -26,18 +89,18 @@ class Mesh(object):
             f.write("# Vertices\n")
             for i,(coordinate,index) in enumerate(sorted(self.vertex_dict.items(),key=lambda x:x[1])):
                 assert i+1 == index
-                f.write("v "+str(coordinate[0])+" "+str(coordinate[1])+" "+str(coordinate[2])+"\n") 
+                f.write("v "+str(coordinate[X_COORD])+" "+str(coordinate[Y_COORD])+" "+str(coordinate[Z_COORD])+"\n") 
             f.write("\n\n")
             f.write("# Faces\n")
             for face in self.face_list:
                 f.write("f "+"".join([str(v_index)+" " for v_index in face])+"\n")
 
 def main():
-    ''' # Test code
+    # Test code
     mesh = Mesh()
     mesh.add_face([(0,0,0),(1,0,0),(0,1,0)])
     mesh.save_to_obj_file("./out.obj")
-    '''
+    
 
 if __name__ == '__main__':
     main()
