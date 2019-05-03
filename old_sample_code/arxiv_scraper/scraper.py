b0967d6abc3ff2a8b0794b226cdeba4d7667e9a7
Paul Nguyen <paul.tqh.nguyen@gmail.com>
2019-05-03 14:07:44 -0500

    arXiv scraper - commit of stubs

---
+++ old_sample_code/arxiv_scraper/scraper.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+
+"""
+
+arXiv Scraping Utilities
+
+Owner : paul-tqh-nguyen
+
+Created : 05/03/2019
+
+File Organization:
+* arXiv Scraping Utilities
+
+"""
+
+import BeautifulSoup
+import urlopen
+
+############################
+# arXiv Scraping Utilities #
+############################
+
+def arxiv_recent_page_titles_and_links():
+    '''@return dictionary of field titles mapped to their "recent pages" links
+       Returns a dictionary of fields, e.g. Statistics and Quantitative Finance, mapped to their "recent papers" page links. Example links include https://arxiv.org/list/eess/recent'''
+    mapping = dict()
+    # stub
+    return mapping
