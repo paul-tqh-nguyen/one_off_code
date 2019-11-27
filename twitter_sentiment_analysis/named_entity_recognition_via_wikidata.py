57863fecf1c858762d26e9222ab94c775893ea7f
paul-tqh-nguyen <paul.tqh.nguyen@gmail.com>
2019-11-27 14:08:38 -0600

    update

--- twitter_sentiment_analysis/named_entity_recognition_via_wikidata.py
+++ twitter_sentiment_analysis/named_entity_recognition_via_wikidata.py
@@ -45,6 +45,7 @@ def _execute_async_task(task):
         asyncio.set_event_loop(event_loop)
         results = event_loop.run_until_complete(asyncio.gather(task))
     except Exception as err:
+        print("_execute_async_task err : {}".format(err))
         pass
     finally:
         event_loop.close()
@@ -119,9 +120,9 @@ async def _most_relevant_wikidata_entities_corresponding_to_string(input_string:
         await browser.close()
     return wikidata_entities_corresponding_to_string
 
-def string_corresponding_commonly_known_entities(input_string: str) -> List[str]:
+def _string_corresponding_commonly_known_entities(input_string: str) -> List[str]:
     print()
-    print("string_corresponding_commonly_known_entities")
+    print("_string_corresponding_commonly_known_entities")
     print("input_string {}".format(input_string))
     task = _most_relevant_wikidata_entities_corresponding_to_string(input_string)
     result = _execute_async_task(task)
@@ -154,61 +155,78 @@ WHERE
 
 WIKI_DATA_QUERY_SERVICE_URI = 'https://query.wikidata.org'
 
-async def _find_commonly_known_isas_via_web_scraper(term_ids_without_item_prefix:str) -> Set[Tuple[str, str]]:
+def _sparql_query_queried_variables(sparql_query:str) -> List[str]:
+    queried_variables = []
+    sparql_tokens = sparql_query.split()
+    assert sparql_tokens[0].lower()=='select'
+    for sparql_token in sparql_tokens[1:]:
+        if sparql_token[0]=='?':
+            queried_variables.append(sparql_token)
+        else:
+            break
+    return queried_variables
+
+async def _query_wikidata_via_web_scraper(sparql_query:str) -> List[dict]:
+    results = []
+    sparql_query_encoded = urllib.parse.quote(sparql_query)
+    uri = WIKI_DATA_QUERY_SERVICE_URI+'/#'+sparql_query_encoded
+    browser = await pyppeteer.launch({'headless': True})
+    page = await browser.newPage()
+    sparql_query_queried_variables = _sparql_query_queried_variables(sparql_query)
+    number_of_variables_queried = len(sparql_query_queried_variables)
+    try:
+        await page.goto(uri)
+        selector_query_for_arbitrary_text_inside_query_box = 'span.cm-variable-2'
+        await page.waitForSelector(selector_query_for_arbitrary_text_inside_query_box)
+        button = await page.querySelector('button#execute-button')
+        await page.evaluate('(button) => button.click()', button)
+        await page.waitForSelector('table.table.table-bordered.table-hover')
+        column_header_divs = await page.querySelectorAll('div.th-inner.sortable.both')
+        assert len(column_header_divs) == number_of_variables_queried
+        variable_names = []
+        for column_header_div in column_header_divs:
+            variable_name = await page.evaluate('(column_header_div) => column_header_div.textContent', column_header_div)
+            variable_names.append(variable_name)
+        assert sparql_query_queried_variables == list(map(lambda variable_name: '?'+variable_name, variable_names))
+        anchors = await page.querySelectorAll('a.item-link')
+        result = dict()
+        for anchor_index, anchor in enumerate(anchors):
+            anchor_variable = variable_names[anchor_index%number_of_variables_queried]
+            anchor_link = await page.evaluate('(anchor) => anchor.href', anchor)
+            assert len(re.findall(r"^http://www.wikidata.org/entity/\w+$", anchor_link))==1
+            entity_id = anchor_link.replace('http://www.wikidata.org/entity/','')
+            anchor_variable_with_question_mark_prefix = '?'+anchor_variable
+            result[anchor_variable_with_question_mark_prefix] = entity_id
+            if (anchor_index%number_of_variables_queried==0 and anchor_index!=0) or len(anchors)-1 == anchor_index:
+                results.append(result)
+                result = dict()
+    except pyppeteer.errors.NetworkError:
+        pass
+    finally:
+        await browser.close()
+    return results
+
+def execute_sparql_query_via_wikidata(sparql_query:str) -> List[dict]:
+    task = _query_wikidata_via_web_scraper(sparql_query)
+    result = _execute_async_task(task)
+    return result
+
+def _find_commonly_known_isas(term_ids: List[str]) -> Set[Tuple[str, str]]:
+    print()
+    print("_find_commonly_known_isas")
+    print("term_ids {}".format(term_ids))
     term_type_id_pairs = set()
     if len(term_ids_without_item_prefix) != 0:
         term_ids = map(lambda raw_term_id: 'wd:'+raw_term_id, term_ids_without_item_prefix)
         space_separated_term_ids = ' '.join(term_ids)
         sparql_query = QUERY_TEMPLATE_FOR_ENTITY_COMMONLY_KNOWN_ISAS.format(space_separated_term_ids=space_separated_term_ids)
-        sparql_query_encoded = urllib.parse.quote(sparql_query)
-        number_of_variables_queried = 2
-        uri = WIKI_DATA_QUERY_SERVICE_URI+'/#'+sparql_query_encoded
-        browser = await pyppeteer.launch({'headless': True})
-        page = await browser.newPage()
-        try:
-            await page.goto(uri)
-            selector_query_for_arbitrary_text_inside_query_box = 'span.cm-variable-2'
-            await page.waitForSelector(selector_query_for_arbitrary_text_inside_query_box)
-            button = await page.querySelector('button#execute-button')
-            await page.evaluate('(button) => button.click()', button)
-            await page.waitForSelector('table.table.table-bordered.table-hover')
-            column_header_divs = await page.querySelectorAll('div.th-inner.sortable.both')
-            assert len(column_header_divs) == number_of_variables_queried
-            variable_names = []
-            for column_header_div in column_header_divs:
-                variable_name = await page.evaluate('(column_header_div) => column_header_div.textContent', column_header_div)
-                variable_names.append(variable_name)
-            anchors = await page.querySelectorAll('a.item-link')
-            current_term = None
-            current_type = None
-            for anchor_index, anchor in enumerate(anchors):
-                anchor_variable = variable_names[anchor_index%number_of_variables_queried]
-                anchor_link = await page.evaluate('(anchor) => anchor.href', anchor)
-                assert len(re.findall(r"^http://www.wikidata.org/entity/\w+$", anchor_link))==1
-                entity_id = anchor_link.replace('http://www.wikidata.org/entity/','')
-                if anchor_variable=='TERM':
-                    current_term = entity_id
-                elif anchor_variable=='VALID_GENLS':
-                    current_type = entity_id
-                else:
-                    raise Exception("Unexpected variable name {unexpected_variable_name}".format(unexpected_variable_name=anchor_variable))
-                if bool(current_term) and bool(current_type):
-                    term_type_id_pairs.add((current_term, current_type))
-                    current_term = None
-                    current_type = None
-        except pyppeteer.errors.NetworkError:
-            pass
-        finally:
-            await browser.close()
+        results = execute_sparql_query_via_wikidata(sparql_query)
+        for result in results:
+            term_type = result['?VALID_GENLS']
+            term_id = result['?TERM_ID']
+            term_type_id_pair = (term_type, term_id)
+            term_type_id_pairs.append(term_type_id_pair)
     return term_type_id_pairs
-
-def find_commonly_known_isas(term_ids: List[str]) -> Set[Tuple[str, str]]:
-    print()
-    print("find_commonly_known_isas")
-    print("term_ids {}".format(term_ids))
-    task = _find_commonly_known_isas_via_web_scraper(term_ids)
-    result = _execute_async_task(task)
-    return result
 
 ###########################
 # Most Abstract Interface #
@@ -216,13 +234,13 @@ def find_commonly_known_isas(term_ids: List[str]) -> Set[Tuple[str, str]]:
 
 @lru_cache(maxsize=32768)
 def string_corresponding_wikidata_term_type_pairs(input_string: str) -> Set[Tuple[str, str]]:
-    term_ids = string_corresponding_commonly_known_entities(input_string)
-    term_type_id_pairs = find_commonly_known_isas(term_ids)
+    term_ids = _string_corresponding_commonly_known_entities(input_string)
+    term_type_id_pairs = _find_commonly_known_isas(term_ids)
     term_type_pairs = [(term, TYPE_TO_ID_MAPPING.inverse[type_id]) for term, type_id in term_type_id_pairs]
     return term_type_pairs
 
 def main():
     print("This module contains utilities for named entity recognition via Wikidata scraping.")
 
-if __name__ == '__main__':
+if _name_ == '_main_':
     main()
