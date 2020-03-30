01bd405ce3fd91c152840107d2aea14632167aca
Paul Nguyen <pnguyen@0584.local>
2020-03-30 15:03:44 -0500

    Update .bashrc and .emacs and python_startup.py files. Timestamp: 15:01:10

--- python_startup.py
+++ python_startup.py
@@ -20,7 +20,7 @@ def print_header() -> None:
 
 import metagraph_cuda.tests.algorithms.test_pagerank ; from importlib import reload ; reload(metagraph_cuda.tests.algorithms.test_pagerank) ; from metagraph_cuda.tests.algorithms.test_pagerank import *
 
-os.chdir(os.path.expanduser('~/code/one_off_code/cugraph_experiments/')); import test_community, importlib; importlib.reload(test_community); from test_community import * ; test_subgraph_extraction()
+os.chdir(os.path.expanduser('~/code/one_off_code/cugraph_experiments/')); import test_community, importlib; importlib.reload(test_community); from test_community import * 
 
 @debug_on_error
 def test():
