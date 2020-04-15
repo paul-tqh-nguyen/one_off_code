ec0e5378247dceb41d2849be5a98e917b8d3f79d
paul-tqh-nguyen <paul.tqh.nguyen@gmail.com>
2020-04-15 18:54:11 -0500

    fix regex

--- d3/bar_chart/index.js
+++ d3/bar_chart/index.js
@@ -5,13 +5,31 @@ svg.style('background-color', 'grey');
 const svg_height = parseFloat(svg.attr('height'));
 const svg_width = parseFloat(svg.attr('width'));
 
-d3.json("location_populations.json")
+// @hack to work around “URL scheme must be ”http“ or ”https“ for CORS request.”
+const data_location = "https://raw.githubusercontent.com/paul-tqh-nguyen/one_off_code/master/d3/bar_chart/location_populations.json"; 
+
+const render = data => {
+    const xScale = d3.scaleLinear()
+          .domain([0, d3.max(data, d=>d.population)]);
+    
+    console.log(xScale.domain());
+    
+    svg.selectAll('rect').data(data)
+        .enter()
+        .append('rect')
+        .attr('width', 300)
+        .attr('height', 300);
+};
+
+d3.json(data_location)
     .then(data => {
         data = data.map(datum => {
-            datum.population = parseFloat(datum.population) * 1000;
-            return datum;
+            return {
+                population: parseFloat(datum.PopTotal) * 1000,
+                location: datum.Location_Populations,
+            };
         });
-        console.log(data);
+        render(data);
     }).catch(err => {
         console.error(err);
         return;
--- d3/bar_chart/preprocess.py
+++ d3/bar_chart/preprocess.py
@@ -2,10 +2,9 @@
 
 import pandas as pd
 
-df = pd.read_csv("./WPP2019_TotalPopulationBySex.csv")
+df = pd.read_csv('./WPP2019_TotalPopulationBySex.csv')
 
 # @hack grabs single word locations only in an attempt to get the country
+shortened_df = df[df.Time==2019][df.Location.str.match(r'^(?!World).*$')][df.Location.str.match(r'^[A-Z][a-z]*$')][['Location','PopTotal']].sort_values('PopTotal', ascending=False).head(10)
 
-shortened_df = df[df.Time==2019][df.Location.str.match(r'^[A-Z][a-z]*$')][['Location','PopTotal']].sort_values('PopTotal', ascending=False).head(10).to_json("location_populations.json", orient='records')
-
-shortened_df.to_csv("./location_populations.csv", index=False)
+shortened_df.to_json('./location_populations.json', orient='records')
