8e0d7f46cbace41ca6f43df5c058b2b4ed633ee5
paul-tqh-nguyen <paul.tqh.nguyen@gmail.com>
2020-04-15 21:33:14 -0500

    char progress

--- d3/bar_chart/index.js
+++ d3/bar_chart/index.js
@@ -6,17 +6,18 @@ const svg_height = parseFloat(svg.attr('height'));
 const svg_width = parseFloat(svg.attr('width'));
 
 // @hack to work around “URL scheme must be ”http“ or ”https“ for CORS request.”
-const data_location = "https://raw.githubusercontent.com/paul-tqh-nguyen/one_off_code/master/d3/bar_chart/location_populations.json"; 
+const data_location = "https://raw.githubusercontent.com/paul-tqh-nguyen/one_off_code/b93f123048a5b0797512ee5fad3c962ba0c3b0d7/d3/bar_chart/location_populations.json"; 
 
 const render = data => {
     const getDatumPopulation = datum => datum.population;
     const getDatumLocation = datum => datum.location;
     const margin = {
-        top: 30,
+        top: 40,
         bottom: 30,
-        left: 80,
+        left: 120,
         right: 30,
     };
+    
     const innerWidth = svg_width - margin.left - margin.right;
     const innerHeight = svg_height - margin.top - margin.bottom;
     
@@ -29,22 +30,38 @@ const render = data => {
           .range([0, innerHeight])
           .padding(0.1);
     
-    const group = svg.append('g')
+    const barChartGroup = svg.append('g')
           .attr('transform', `translate(${margin.left}, ${margin.top})`);
-    
-    const yAxisGroup = group.append('g')
+
+    const barChartTitle = barChartGroup.append('text')
+          .text("Some Populous Countries")
+          .attr('x', 5)
+          .attr('y', -10);
+        
+    const yAxisGroup = barChartGroup.append('g')
           .call(d3.axisLeft(yScale));
     
-    const xAxisGroup = group.append('g')
-          .call(d3.axisBottom(xScale))
-          .attr('transform', `translate(0, ${innerHeight})`);
+    yAxisGroup
+        .selectAll('.domain, .tick line')
+        .remove();
     
-    group.selectAll('rect').data(data)
+    const xAxisTickFormat = number => d3.format('.3s')(number).replace(/G/,"B");
+    
+    const xAxisGroup = barChartGroup.append('g')
+          .call(d3.axisBottom(xScale).tickFormat(xAxisTickFormat))
+          .attr('transform', `translate(0, ${innerHeight})`);
+
+    xAxisGroup
+        .selectAll('.domain')
+        .remove();
+    
+    barChartGroup.selectAll('rect').data(data)
         .enter()
         .append('rect')
         .attr('width', datum => xScale(getDatumPopulation(datum)))
         .attr('y', datum => yScale(getDatumLocation(datum)))
         .attr('height', yScale.bandwidth());
+
 };
 
 d3.json(data_location)
