361523a36bea71f829465d70c2bb7c18762630b2
paul-tqh-nguyen <paul.tqh.nguyen@gmail.com>
2020-04-21 00:06:45 -0500

    Update .bashrc and .emacs and python_startup.py files. Timestamp: 23:34:57

--- netflix/preprocess.py
+++ netflix/preprocess.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 
+import math
+import multiprocessing
 import pandas as pd
 import networkx as nx
 import matplotlib
@@ -14,20 +16,18 @@ RAW_DATA_CSV = './netflix_titles.csv'
 DIRECTOR_ACTOR_EDGE_LIST_CSV = './director_actor_edge_list.csv'
 PROJECTED_ACTORS_CSV = './projected_actors.csv'
 PROJECTED_DIRECTORS_CSV = './projected_directors.csv'
-PROJECTED_ACTORS_PNG = './projected_actors.png'
-PROJECTED_DIRECTORS_PNG = './projected_directors.png'
-
-MAXIMUM_NUMBER_OF_ACTORS = 500 # 5000
-MAXIMUM_NUMBER_OF_DIRECTORS = 500 # 5000
 
 RELEVANT_COLUMNS = ['title', 'cast', 'director', 'country', 'type', 'listed_in']
 COLUMNS_WITH_LIST_VALUES_WORTH_EXPANDING = ['cast', 'director']
 assert set(COLUMNS_WITH_LIST_VALUES_WORTH_EXPANDING).issubset(RELEVANT_COLUMNS)
 
+NODE_COUNT_UPPER_BOUNDS = [100, 1000, 5000, 10_000, 15_000]
+NODE_COUNT_UPPER_BOUNDS.reverse()
+
 def draw_graph_to_file(graph: nx.Graph, output_location: str) -> None:
     with temp_plt_figure(figsize=(20.0,10.0)) as figure:
         plot = figure.add_subplot(111)
-        layout = nx.spring_layout(graph, iterations=15)
+        layout = nx.spring_layout(graph, iterations=max(15, math.sqrt(graph.nodes)))
         nx.draw_networkx_edges(graph,
                                layout, 
                                width=1,
@@ -50,37 +50,40 @@ def expand_dataframe_list_values_for_column(df: pd.DataFrame, column_name: str) 
                   .join(df.drop(columns=[column_name]))
 
 def reduce_graph_size_via_increasing_k_core(graph: nx.Graph, node_count_upperbound: int) -> nx.Graph:
-    degrees = dict(graph.degree).values()
-    degree_histogram = histogram(degrees)
-    sorted_degree_histogram = sorted(degree_histogram.items(), key=lambda x: -x[0])
-    current_sum = 0
-    for (degree, number_of_nodes_with_degree) in sorted_degree_histogram:
-        current_sum += number_of_nodes_with_degree
-        if current_sum >= node_count_upperbound:
-            k_core_guess = degree/2
-            break
-    k_lower_bound = 0
-    k_upper_bound = sorted_degree_histogram[0][0]
-    bigger_graph = graph
-    while True:
-        reduced_graph = nx.k_core(bigger_graph, k_core_guess)
-        if len(reduced_graph.nodes) < node_count_upperbound:
-            k_upper_bound = k_core_guess
-            new_k_core_guess = (k_core_guess + k_lower_bound) // 2
-            if new_k_core_guess == k_lower_bound:
+    if len(graph)<node_count_upperbound:
+        reduced_graph = graph
+    else:
+        degrees = dict(graph.degree).values()
+        degree_histogram = histogram(degrees)
+        sorted_degree_histogram = sorted(degree_histogram.items(), key=lambda x: -x[0])
+        current_sum = 0
+        for (degree, number_of_nodes_with_degree) in sorted_degree_histogram:
+            current_sum += number_of_nodes_with_degree
+            if current_sum >= node_count_upperbound:
+                k_core_guess = degree/2
                 break
-            k_core_guess = new_k_core_guess
-        elif len(reduced_graph.nodes) > node_count_upperbound:
-            bigger_graph = reduced_graph
-            k_lower_bound = k_core_guess
-            new_k_core_guess = (k_core_guess + k_upper_bound) // 2
-            if k_core_guess == new_k_core_guess:
-                k_core_guess += 1
-                reduced_graph = nx.k_core(bigger_graph, k_core_guess)
+        k_lower_bound = 0
+        k_upper_bound = sorted_degree_histogram[0][0]
+        bigger_graph = graph
+        while True:
+            reduced_graph = nx.k_core(bigger_graph, k_core_guess)
+            if len(reduced_graph.nodes) < node_count_upperbound:
+                k_upper_bound = k_core_guess
+                new_k_core_guess = (k_core_guess + k_lower_bound) // 2
+                if new_k_core_guess == k_lower_bound:
+                    break
+                k_core_guess = new_k_core_guess
+            elif len(reduced_graph.nodes) > node_count_upperbound:
+                bigger_graph = reduced_graph
+                k_lower_bound = k_core_guess
+                new_k_core_guess = (k_core_guess + k_upper_bound) // 2
+                if k_core_guess == new_k_core_guess:
+                    k_core_guess += 1
+                    reduced_graph = nx.k_core(bigger_graph, k_core_guess)
+                    break
+                k_core_guess = new_k_core_guess
+            else:
                 break
-            k_core_guess = new_k_core_guess
-        else:
-            break
     return reduced_graph
 
 def preprocess_data() -> None:
@@ -96,23 +99,31 @@ def preprocess_data() -> None:
         movies_df.to_csv(DIRECTOR_ACTOR_EDGE_LIST_CSV, index=False)
         movies_graph = nx.from_pandas_edgelist(movies_df, 'cast', 'director', True)
     with timer(section_name="Actor graph projection"):
-        projected_actors_graph = nx.projected_graph(movies_graph, movies_df['cast'])
-        projected_actors_graph = reduce_graph_size_via_increasing_k_core(projected_actors_graph, MAXIMUM_NUMBER_OF_ACTORS)
-        projected_actors_edgelist = nx.convert_matrix.to_pandas_edgelist(projected_actors_graph)
+        full_projected_actors_graph = nx.projected_graph(movies_graph, movies_df['cast'])
+        projected_actors_edgelist = nx.convert_matrix.to_pandas_edgelist(full_projected_actors_graph)
         projected_actors_edgelist.rename(columns={"source": "source_actor", "target": "target_actor"})
         projected_actors_edgelist.to_csv(PROJECTED_ACTORS_CSV, index=False)
         print(f"Number of Actors: {len(projected_actors_graph.nodes)}")
-    with timer(section_name="Actor graph projection visualization"):
-        draw_graph_to_file(projected_actors_graph, PROJECTED_ACTORS_PNG)
     with timer(section_name="Director graph projection"):
-        projected_directors_graph = nx.projected_graph(movies_graph, movies_df['director'])
-        projected_directors_graph = reduce_graph_size_via_increasing_k_core(projected_directors_graph, MAXIMUM_NUMBER_OF_DIRECTORS)
-        projected_directors_edgelist = nx.convert_matrix.to_pandas_edgelist(projected_directors_graph)
+        full_projected_directors_graph = nx.projected_graph(movies_graph, movies_df['director'])
+        projected_directors_edgelist = nx.convert_matrix.to_pandas_edgelist(full_projected_directors_graph)
         projected_directors_edgelist.rename(columns={"source": "source_director", "target": "target_director"})
         projected_directors_edgelist.to_csv(PROJECTED_DIRECTORS_CSV, index=False)
         print(f"Number of Directors: {len(projected_directors_graph.nodes)}")
-    with timer(section_name="Director graph projection visualization"):
-        draw_graph_to_file(projected_directors_graph, PROJECTED_DIRECTORS_PNG)
+    with timer(section_name="Actor/Director graph projection visualization"):
+        def visualize_actors(maximum_number_of_directors: int) -> None:
+            projected_actors_graph = reduce_graph_size_via_increasing_k_core(full_projected_actors_graph, maximum_number_of_actors)
+            draw_graph_to_file(projected_actors_graph, f'./projected_actors_{maximum_number_of_actors}.png')
+            return
+        def visualize_directors(maximum_number_of_directors: int) -> None:
+            projected_directors_graph = reduce_graph_size_via_increasing_k_core(full_projected_directors_graph, maximum_number_of_directors)
+            draw_graph_to_file(projected_directors_graph, f'./projected_directors_{maximum_number_of_directors}.png')
+            return
+        p = multiprocessing.Pool()
+        p.map(visualize_actors, NODE_COUNT_UPPER_BOUNDS)
+        p.map(visualize_directors, NODE_COUNT_UPPER_BOUNDS)
+        p.close()
+        p.join()
     return
 
 @debug_on_error
