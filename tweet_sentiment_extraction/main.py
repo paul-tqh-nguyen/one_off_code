88f15906698bdf86e3b3ff27548fc112915fdb97
Paul Nguyen <paul.tqh.nguyen@gmail.com>
2020-05-15 10:00:06 -0700

    updates

--- tweet_sentiment_extraction/main.py
+++ tweet_sentiment_extraction/main.py
@@ -177,6 +177,8 @@ def NaiveDensePredictor_generator() -> Generator:
         [110, 64, 32, 16, 1],
         [110, 64, 32, 16, 8, 1],
         [110, 64, 32, 16, 8, 4, 1],
+        [110, 32, 1],
+        [110, 32, 8, 1],
     ]
     dropout_probability_choices = [0.5]
 
@@ -192,7 +194,7 @@ def NaiveDensePredictor_generator() -> Generator:
     random.seed()
     random.shuffle(hyparameter_list_choices)
     for (batch_size, max_vocab_size, pre_trained_embedding_specification, loss_function_spec, sentiment_size, dense_sequence_lengths, dropout_probability) in hyparameter_list_choices:
-        output_directory = f'./results/NaiveDensePredictor_batch_{batch_size}_train_frac_{train_portion}_valid_frac_{validation_portion}_vocab_{max_vocab_size}_embed_spec_{pre_trained_embedding_specification}_{loss_function_spec}_sentiment_size_{sentiment_size}_dense_lens_{dense_sequence_lengths}_dropout_{dropout_probability}'
+        output_directory = f'./results/NaiveDensePredictor_batch_{batch_size}_train_frac_{train_portion}_valid_frac_{validation_portion}_vocab_{max_vocab_size}_embed_spec_{pre_trained_embedding_specification}_{loss_function_spec}_sentiment_size_{sentiment_size}_dense_lens_{str(dense_sequence_lengths).replace(" ","")}_dropout_{dropout_probability}'
         final_output_results_file = os.path.join(output_directory, FINAL_MODEL_SCORE_JSON_FILE_BASE_NAME)
         if os.path.isfile(final_output_results_file):
             print(f'Skipping result generation for {final_output_results_file}.')
@@ -204,12 +206,7 @@ def NaiveDensePredictor_generator() -> Generator:
                                             dropout_probability=dropout_probability)
             yield predictor
 
-def hyperparameter_search_word_selector_models() -> None:
-    predictors = roundrobin(
-        # LSTMSentimentConcatenationPredictor_generator(),
-        LSTMScaledDotProductAttentionPredictor_generator(),
-        NaiveDensePredictor_generator(),
-    )
+def hyperparameter_search(predictors: Iterable) -> None:
     for predictor in predictors:
         with safe_cuda_memory():
             predictor.train()
@@ -223,7 +220,12 @@ def main() -> None:
     parser = argparse.ArgumentParser(formatter_class = lambda prog: argparse.HelpFormatter(prog, max_help_position = 9999))
     parser.add_argument('-preprocess-data', action='store_true', help=f'Preprocess the raw data. Results stored in {PREPROCESSED_TRAINING_DATA_JSON_FILE}.')
     parser.add_argument('-train-word-selector-model', action='store_true', help=f'Trains & evaluates our word selector model on our preprocessed dataset. Results are saved to {WORD_SELECTOR_RESULTS_DIRECTORY}.')
-    parser.add_argument('-hyperparameter-search-word-selector-models', action='store_true', help=f'Exhaustively performs hyperparameter random search. Details of the best performance are tracked in {WORD_SELECTOR_BEST_MODEL_SCORE_JSON_FILE_LOCATION}.')
+    parser.add_argument('-hyperparameter-search-word-selector-models', action='store_true',
+                        help=f'Exhaustively performs hyperparameter random search. Details of the best performance are tracked in {WORD_SELECTOR_BEST_MODEL_SCORE_JSON_FILE_LOCATION}.')
+    parser.add_argument('-hyperparameter-search-lstm-sentiment-concatenation-predictor', action='store_true', help=f'Exhaustively performs hyperparameter random search using only the LSTMSentimentConcatenationPredictor model.')
+    parser.add_argument('-hyperparameter-search-lstm-scaled-dot-product-attention-predictor', action='store_true',
+                        help=f'Exhaustively performs hyperparameter random search using only the LSTMScaledDotProductAttentionPredictor model.')
+    parser.add_argument('-hyperparameter-search-naive-dense-predictor', action='store_true', help=f'Exhaustively performs hyperparameter random search using only the NaiveDensePredictor model.')
     args = parser.parse_args()
     number_of_args_specified = sum(map(int,vars(args).values()))
     if number_of_args_specified == 0:
@@ -237,7 +239,21 @@ def main() -> None:
         from word_selector_models.models import train_model
         train_model()
     elif args.hyperparameter_search_word_selector_models:
-        hyperparameter_search_word_selector_models()
+        predictors = roundrobin(
+            LSTMSentimentConcatenationPredictor_generator(),
+            LSTMScaledDotProductAttentionPredictor_generator(),
+            NaiveDensePredictor_generator(),
+        )
+        hyperparameter_search(predictors)
+    elif args.hyperparameter_search_lstm_sentiment_concatenation_predictor:
+        predictors = LSTMSentimentConcatenationPredictor_generator()
+        hyperparameter_search(predictors)
+    elif args.hyperparameter_search_lstm_scaled_dot_product_attention_predictor:
+        predictors = LSTMScaledDotProductAttentionPredictor_generator()
+        hyperparameter_search(predictors)
+    elif args.hyperparameter_search_naive_dense_predictor:
+        predictors = NaiveDensePredictor_generator()
+        hyperparameter_search(predictors)
     else:
         raise Exception('Unexpected args received.')
     return
