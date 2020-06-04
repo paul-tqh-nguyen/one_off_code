add19ed4e615e688d7541a36f791ce91390d5e67
Paul Nguyen <pnguyen@0584.local>
2020-06-04 17:30:54 -0500

    checkpointing

---
+++ hierarchy_visualization/gather_data.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+'#!/usr/bin/python3 -OO'
+
+"""
+"""
+
+# @todo update doc string
+
+###########
+# Imports #
+###########
+
+import bs4
+
+from misc_utilities import *
+
+###########
+# Globals #
+###########
+
+OUTPUT_CSV_FILE = './data.csv'
+
+START_NODE_LINK = 'https://www.wikidata.org/wiki/Q10884' # tree
+
+##########
+# Driver #
+##########
+
+def gather_data() -> None:
+    
+    return
+
+if __name__ == '__main__':
+    gather_data()
