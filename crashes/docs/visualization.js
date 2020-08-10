a1bd16ba1a8902cffc1857d6692884587328346b
Paul Nguyen <pnguyen@0584.attlocal.net>
2020-08-09 23:55:50 -0500

    fix crash dot colors

--- crashes/docs/visualization.js
+++ crashes/docs/visualization.js
@@ -7,8 +7,61 @@ const capitalizeString  = str => {
         splitStr[i] = splitStr[i].charAt(0).toUpperCase() + splitStr[i].substring(1);
     }
     return splitStr.join(' '); 
-}
+};
 
+const hexCompliment = hex => {
+    let [r,g,b] = ('rgb(' + (hex = hex.replace('#', '')).match(new RegExp('(.{' + hex.length/3 + '})', 'g')).map(function(l) { return parseInt(hex.length%2 ? l+l : l, 16); }).join(',') + ')')
+          .replace(/[^\d,]/g, '').split(',').map(intensity => intensity / 255.0);
+    const max = Math.max(r, g, b);
+    const min = Math.min(r, g, b);
+    let h, s, l = (max + min) / 2.0;
+    if (max === min) {
+        h = s = 0;
+    } else {
+        const d = max - min;
+        s = (l > 0.5 ? d / (2.0 - max - min) : d / (max + min));
+        if(max == r && g >= b) {
+            h = 1.0472 * (g - b) / d ;
+        } else if(max == r && g < b) {
+            h = 1.0472 * (g - b) / d + 6.2832;
+        } else if(max == g) {
+            h = 1.0472 * (b - r) / d + 2.0944;
+        } else if(max == b) {
+            h = 1.0472 * (r - g) / d + 4.1888;
+        }
+    }
+    h = h / 6.2832 * 360.0 + 0;
+    h += 180;
+    if (h > 360) {
+        h -= 360;
+    }
+    h /= 360;
+    if(s === 0){
+        r = g = b = l;
+    } else {
+        const hue2rgb = (p, q, t) => {
+            if(t < 0) t += 1;
+            if(t > 1) t -= 1;
+            if(t < 1/6)
+                return p + (q - p) * 6 * t;
+            if(t < 1/2)
+                return q;
+            if(t < 2/3)
+                return p + (q - p) * (2/3 - t) * 6;
+            return p;
+        };
+        const q = l < 0.5 ? l * (1 + s) : l + s - l * s;
+        const p = 2 * l - q;
+        r = hue2rgb(p, q, h + 1/3);
+        g = hue2rgb(p, q, h);
+        b = hue2rgb(p, q, h - 1/3);
+    }
+    r = Math.round(r * 255);
+    g = Math.round(g * 255); 
+    b = Math.round(b * 255);
+    const rgb = b | (g << 8) | (r << 16); 
+    return "#" + (0x1000000 | rgb).toString(16).substring(1);
+};
 
 d3.selection.prototype.moveToFront = function() {
     return this.each(function() {
@@ -256,6 +309,7 @@ const visualizationMain = () => {
                         .attr('cx', datum => datum.x)
                         .attr('cy', datum => datum.y)
                         .attr('class', 'crash')
+                        .attr('fill', datum => hexCompliment(boroughToHexColor[capitalizeString(datum.BOROUGH)]))
                         .on('mouseover', datum => {
                             if (zoomLevel === zoomLevels.ZIPCODE) {
                                 const toolTipX = d3.event.pageX;
