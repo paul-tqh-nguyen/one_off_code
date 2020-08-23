4e14b9b8426612791d808c6f66e95a5349b7b984
Paul Nguyen <pnguyen@0584.attlocal.net>
2020-08-23 17:33:10 -0500

    checkpoint

--- nyc_get_food/main.py
+++ nyc_get_food/main.py
@@ -1,7 +1,7 @@
 '#!/usr/bin/python3 -OO' # @todo enable this
 
-"""
-"""
+'''
+'''
 
 # @todo update doc string
 
@@ -14,9 +14,13 @@ import psutil
 import asyncio
 import pyppeteer
 import itertools
+import queue
 import time
 import warnings
+import bs4
+import random
 import pandas as pd
+import multiprocessing as mp
 from statistics import mean
 from typing import Union, List, Tuple, Iterable, Callable, Awaitable
 
@@ -28,24 +32,15 @@ from misc_utilities import *
 # Globals #
 ###########
 
-MAX_NUMBER_OF_NEW_PAGE_ATTEMPTS = 1000
-NUMBER_OF_ATTEMPTS_PER_SLEEP = 1
-SLEEPING_RANGE_SLEEP_TIME = 10
-BROWSER_IS_HEADLESS = False
+BROWSER_IS_HEADLESS = False # @hack clicking has problems when headless
 
-TARGET_URL = "https://dsny.maps.arcgis.com/apps/webappviewer/index.html?id=35901167a9d84fb0a2e0672d344f176f"
+TARGET_URL = 'https://dsny.maps.arcgis.com/apps/webappviewer/index.html?id=35901167a9d84fb0a2e0672d344f176f'
 
 OUTPUT_CSV_FILE = './raw_data.csv'
 
 ##########################
 # Web Scraping Utilities #
 ##########################
-
-def _sleeping_range(upper_bound: int):
-    for attempt_index in range(upper_bound):
-        if attempt_index and attempt_index % NUMBER_OF_ATTEMPTS_PER_SLEEP == 0:
-            time.sleep(SLEEPING_RANGE_SLEEP_TIME*(attempt_index//NUMBER_OF_ATTEMPTS_PER_SLEEP))
-        yield attempt_index
 
 EVENT_LOOP = asyncio.new_event_loop()
 asyncio.set_event_loop(EVENT_LOOP)
@@ -63,38 +58,12 @@ BROWSER = EVENT_LOOP.run_until_complete(_launch_browser())
 
 def scrape_function(func: Awaitable) -> Awaitable:
     async def decorating_function(*args, **kwargs):
-        unique_bogus_result_identifier = object()
-        result = unique_bogus_result_identifier
         global BROWSER
-        for _ in _sleeping_range(MAX_NUMBER_OF_NEW_PAGE_ATTEMPTS):
-            try:
-                updated_kwargs = kwargs.copy()
-                pages = await BROWSER.pages()
-                page = pages[-1]
-                updated_kwargs['page'] = page
-                result = await func(*args, **updated_kwargs)
-            except (pyppeteer.errors.BrowserError,
-                    pyppeteer.errors.ElementHandleError,
-                    pyppeteer.errors.NetworkError,
-                    pyppeteer.errors.PageError,
-                    pyppeteer.errors.PyppeteerError) as err:
-                warnings.warn(f'\n{time.strftime("%m/%d/%Y_%H:%M:%S")} {func.__name__} {err}')
-                warnings.warn(f'\n{time.strftime("%m/%d/%Y_%H:%M:%S")} Launching new page.')
-                await BROWSER.newPage()
-            except pyppeteer.errors.TimeoutError as err:
-                warnings.warn(f'\n{time.strftime("%m/%d/%Y_%H:%M:%S")} {func.__name__} {err}')
-                warnings.warn(f'\n{time.strftime("%m/%d/%Y_%H:%M:%S")} Launching new browser.')
-                browser_process = only_one([process for process in psutil.process_iter() if process.pid==BROWSER.process.pid])
-                for child_process in browser_process.children(recursive=True):
-                    child_process.kill()
-                browser_process.kill() # @hack memory leak ; this line doesn't actually kill the process (or maybe it just doesn't free the PID?)
-                BROWSER = await _launch_browser()
-            except Exception as err:
-                raise
-            if result != unique_bogus_result_identifier:
-                break
-        if result == unique_bogus_result_identifier:
-            raise Exception
+        updated_kwargs = kwargs.copy()
+        pages = await BROWSER.pages()
+        page = pages[-1]
+        updated_kwargs['page'] = page
+        result = await func(*args, **updated_kwargs)
         return result
     return decorating_function
 
@@ -115,30 +84,94 @@ async def _get_sole_element(self: Union[pyppeteer.page.Page, pyppeteer.element_h
 setattr(pyppeteer.page.Page, 'get_sole_element', _get_sole_element)
 setattr(pyppeteer.element_handle.ElementHandle, 'get_sole_element', _get_sole_element)
 
+async def _hide_sole_element(self: Union[pyppeteer.page.Page, pyppeteer.element_handle.ElementHandle], selector: str) -> None:
+    element = await self.get_sole_element(selector)
+    await self.evaluate('(element) => {element.style.display = "none";}', element)
+    return 
+setattr(pyppeteer.page.Page, 'hide_sole_element', _hide_sole_element)
+setattr(pyppeteer.element_handle.ElementHandle, 'hide_sole_element', _hide_sole_element)
+
+async def _remove_display_style_on_sole_element(self: Union[pyppeteer.page.Page, pyppeteer.element_handle.ElementHandle], selector: str) -> None:
+    element = await self.get_sole_element(selector)
+    await self.evaluate('(element) => {element.style.display = null;}', element)
+    return 
+setattr(pyppeteer.page.Page, 'remove_display_style_on_sole_element', _remove_display_style_on_sole_element)
+setattr(pyppeteer.element_handle.ElementHandle, 'remove_display_style_on_sole_element', _remove_display_style_on_sole_element)
+
 ########################
 # Get Food NYC Scraper #
 ########################
 
-async def hide_welcome_div(page: pyppeteer.page.Page) -> None:
-    welcome_div = await page.get_sole_element('div#_5_panel')
-    await page.evaluate('(element) => {element.style.display = "none"}', welcome_div)
+@trace
+def process_location_html_string(html_string: str) -> dict:
+    soup = bs4.BeautifulSoup(html_string,'html.parser')
+    result = dict(location_type=only_one(soup.findAll('div', {'dojoattachpoint' : '_title'})).text)
+    description_div = only_one(soup.findAll('div', {'dojoattachpoint' : '_description'}))
+    for tag_index, tag in enumerate(description_div.findChildren('font' , recursive=False)):
+        line = tag.text.replace('\xa0', ' ')
+        if ' is located at ' in line:
+            location_name, location_address = [child.text for child in tag.findChildren('font')]
+            result['location_name'] = location_name
+            result['location_address'] = location_address
+            # @todo handle other cases of location_address where it might talk about programs included with the location.
+        else:
+            pass
+            # raise ValueError(f'Unhandled html string:\n{html_string}')
+    return result
+
+async def zoom_to_circle(page: pyppeteer.page.Page, circle: pyppeteer.element_handle.ElementHandle, display_div: pyppeteer.element_handle.ElementHandle) -> None:
+    await circle.click()
+    await page.remove_display_style_on_sole_element('div.esriPopupWrapper')
+    await page.remove_display_style_on_sole_element('div.outerPointer')
+    await circle.click()
+    zoom_to_link = await page.get_sole_element('a.action.zoomTo')
+    await zoom_to_link.click()
+    await page.hide_sole_element('div.esriPopupWrapper')
+    await page.hide_sole_element('div.outerPointer')
     return
 
 @scrape_function
-async def _gather_location(*, page: pyppeteer.page.Page) -> None:
+async def _gather_location_display_df(*, page: pyppeteer.page.Page) -> pd.DataFrame:
+    row_dicts: List[str] = []
     await page.goto(TARGET_URL)
-    await hide_welcome_div(page) # strictly for debugging
+    home_button = await page.get_sole_element('div.home')
+    await home_button.click()
+    await page.hide_sole_element('div#_5_panel')
+    await page.hide_sole_element('div.esriPopupWrapper')
+    initial_zooming_circles = await page.get_elements('g#DOE_active_hub_2222_layer circle')
+    new_zooming_circles_x_y_pairs = {tuple(await page.evaluate('(element) => [element.getAttribute("cx"), element.getAttribute("cy")]', zooming_circle)) for zooming_circle in initial_zooming_circles}
+    await initial_zooming_circles[0].click()
+    await page.hide_sole_element('div.outerPointer')
     display_div = await page.get_sole_element('.esriPopupWrapper')
-    circles = await page.get_elements('g#DOE_active_hub_2222_layer circle')
-    for circle in circles:
-        await circle.click()
-        display_div_html = await page.evaluate('(element) => element.innerHTML', display_div)
-    return None
+    seen_addresses = set()
+    for _ in range(len(new_zooming_circles_x_y_pairs)): # @todo inefficient O(n^3) & redundant to avoid clicking issues via obfuscated circles
+        print(f"_ {repr(_)}")
+        await home_button.click()
+        zooming_circles = await page.get_elements('g#DOE_active_hub_2222_layer circle')
+        for zooming_circle in zooming_circles:
+            x_y_pair = tuple(await page.evaluate('(element) => [element.getAttribute("cx"), element.getAttribute("cy")]', zooming_circle))
+            if x_y_pair not in new_zooming_circles_x_y_pairs:
+                break
+            await zoom_to_circle(page, zooming_circle, display_div)
+            circles = await page.get_elements('g#DOE_active_hub_2222_layer circle')
+            for circle in circles:
+                await circle.click()
+                display_div_html = await page.evaluate('(element) => element.innerHTML', display_div)
+                row_dict = process_location_html_string(display_div_html)
+                location_address = row_dict['location_address']
+                if location_address not in seen_addresses:
+                    row_dicts.append(row_dict)
+                    seen_addresses.add(location_address)
+            new_zooming_circles_x_y_pairs.remove(x_y_pair)
+    assert len(row_dicts) == len(initial_zooming_circles)
+    df = pd.DataFrame(row_dicts)
+    return df
 
-def gather() -> None:
-    results = EVENT_LOOP.run_until_complete(_gather())
-    time.sleep(10)
-    return 
+def gather_location_display_df() -> pd.DataFrame:
+    df = EVENT_LOOP.run_until_complete(_gather_location_display_df())
+    # @todo add lat longs
+    # @todo add boroughs
+    return df
 
 ###################
 # Sanity Checking #
@@ -153,9 +186,8 @@ def sanity_check_output_csv_file() -> None:
 ##########
 
 def gather_data() -> None:
-    with timer("Data gathering"):
-        gather()
-        sanity_check_output_csv_file()
+    with timer('Data gathering'):
+        df = gather_location_display_df()
     return
 
 if __name__ == '__main__':
