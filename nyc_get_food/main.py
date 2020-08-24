3f6e85b45be4b390f92f317feb37e93aa7ca165f
Paul Nguyen <pnguyen@0584.attlocal.net>
2020-08-24 18:08:11 -0500

    Save

--- nyc_get_food/main.py
+++ nyc_get_food/main.py
@@ -11,6 +11,7 @@
 
 import os
 import re
+import json
 import psutil
 import asyncio
 import pyppeteer
@@ -38,7 +39,8 @@ BROWSER_IS_HEADLESS = False # location-based clicking in pyppeteer currently bug
 
 TARGET_URL = 'https://dsny.maps.arcgis.com/apps/webappviewer/index.html?id=35901167a9d84fb0a2e0672d344f176f'
 
-OUTPUT_JSON_FILE = './raw_data.json'
+RAW_SCRAPED_DATA_JSON_FILE = './raw_scraped_data.json'
+OUTPUT_JSON_FILE = './scraped_data.json'
 
 ##########################
 # Web Scraping Utilities #
@@ -102,12 +104,12 @@ def process_location_html_string(raw_html_string: str) -> dict:
             current_tag_into_type = 'halal_meal_availability'
         elif line == 'This site is':
             current_tag_into_type = 'accessibility'
-        elif line in ('.', '', 'To ensure every New York City resident can access nutritious meals, the Department of Education meal hub sites provide three meals a day, Monday through Friday, to both youth and adults in need. There is no registration or identification required.'):
+        elif line in ('.', '', 'which has  programs.', 'To ensure every New York City resident can access nutritious meals, the Department of Education meal hub sites provide three meals a day, Monday through Friday, to both youth and adults in need. There is no registration or identification required.'):
             pass
         elif line.startswith('which has '):
             assert line.endswith(' programs.')
             programs_string = line.replace('which has ', '').replace(' programs.', '').replace(' and ', ', ')
-            assert len(programs_string) > 0, f'{repr(programs_string)} not correctly parsed.'
+            assert len(programs_string) > 0, f'{repr(line)} not correctly parsed.'
             programs = programs_string.split(', ')
             location_dict['location_available_programs'] = programs
             current_tag_into_type = None
@@ -155,7 +157,9 @@ async def _scrape_location_dicts(page: pyppeteer.page.Page) -> List[dict]:
         await home_button.click()
         await page.mouse.click(2 if bool(circle_index%2) else window_width - 5, window_height//2) # avoid double clicking while pinging at sub-double-click speed
         display_div_html_string = await page.evaluate('(element) => element.innerHTML', display_div)
-        location_dicts.append(process_location_html_string(display_div_html_string))
+        location_dict = process_location_html_string(display_div_html_string)
+        sanity_check_location_dict(location_dict)
+        location_dicts.append(location_dict)
         await page.evaluate('(element) => {{element.setAttribute("r", "0px")}}',  circle) # hide processedd circle to avoid obfuscation and overlap
     assert len(location_dicts) == len(circles)
     return location_dicts
@@ -163,7 +167,13 @@ async def _scrape_location_dicts(page: pyppeteer.page.Page) -> List[dict]:
 async def _gather_location_dicts() -> List[dict]:
     browser = await launch_browser()
     page = only_one(await browser.pages())
-    location_dicts = await _scrape_location_dicts(page)
+    if os.path.isfile(RAW_SCRAPED_DATA_JSON_FILE):
+        with open(RAW_SCRAPED_DATA_JSON_FILE, 'w') as json_file_handle:
+            location_dicts = json.load(json_file_handle)
+    else:
+        location_dicts = await _scrape_location_dicts(page)
+        with open(RAW_SCRAPED_DATA_JSON_FILE, 'w') as json_file_handle:
+            json.dump(location_dicts, json_file_handle, indent=4)
     # @todo add lat longs
     # @todo add boroughs
     browser_process = only_one([process for process in psutil.process_iter() if process.pid==browser.process.pid])
@@ -176,20 +186,18 @@ async def _gather_location_dicts() -> List[dict]:
 # Sanity Checking #
 ###################
 
-def sanity_check_location_dicts(location_dicts: List[dict]) -> None:
-    for location_dict in location_dicts:
-        assert len(location_dict) >= 4
-        assert isinstance(location_dict['raw_html_string'], str) and len(location_dict['raw_html_string']) > 0
-        assert isinstance(location_dict['location_type'], str) and len(location_dict['location_type']) > 0
-        assert isinstance(location_dict['location_name'], str) and len(location_dict['location_name']) > 0
-        assert isinstance(location_dict['location_address'], list) and len(location_dict['location_address']) > 0 and all(isinstance(address_line, str) for address_line in location_dict['location_address'])
-        assert implies('location_operating_hours' in location_dict, isinstance(location_dict['location_operating_hours'], str) and len(location_dict['location_operating_hours']) > 0)
-        assert implies('location_accepts_ebt' in location_dict, isinstance(location_dict['location_accepts_ebt'], bool))
-        assert implies('location_halal_meal_availability' in location_dict, isinstance(location_dict['location_halal_meal_availability'], bool))
-        assert implies('location_available_programs' in location_dict, isinstance(location_dict['location_available_programs'], list) and len(location_dict['location_available_programs']) > 0and \
-                       all(isinstance(program, str) and len(program) > 0 for program in location_dict['location_available_programs']))
-        assert implies('location_accessibility' in location_dict, isinstance(location_dict['location_accessibility'], str) and len(location_dict['location_accessibility']) > 0)
-    breakpoint()
+def sanity_check_location_dict(location_dict: dict) -> None:
+    assert len(location_dict) >= 4
+    assert isinstance(location_dict['raw_html_string'], str) and len(location_dict['raw_html_string']) > 0
+    assert isinstance(location_dict['location_type'], str) and len(location_dict['location_type']) > 0
+    assert isinstance(location_dict['location_name'], str) and len(location_dict['location_name']) > 0
+    assert isinstance(location_dict['location_address'], list) and len(location_dict['location_address']) > 0 and all(isinstance(address_line, str) for address_line in location_dict['location_address'])
+    assert implies('location_operating_hours' in location_dict, isinstance(location_dict.get('location_operating_hours'), list) and len(location_dict.get('location_operating_hours')) > 0)
+    assert implies('location_accepts_ebt' in location_dict, isinstance(location_dict.get('location_accepts_ebt'), bool))
+    assert implies('location_halal_meal_availability' in location_dict, isinstance(location_dict.get('location_halal_meal_availability'), bool))
+    assert implies('location_available_programs' in location_dict, isinstance(location_dict.get('location_available_programs'), list) and len(location_dict.get('location_available_programs')) > 0 and \
+                   all(isinstance(program, str) and len(program) > 0 for program in location_dict.get('location_available_programs')))
+    assert implies('location_accessibility' in location_dict, isinstance(location_dict.get('location_accessibility'), str) and len(location_dict.get('location_accessibility')) > 0)
     return 
 
 ##########
@@ -199,9 +207,9 @@ def sanity_check_location_dicts(location_dicts: List[dict]) -> None:
 def gather_data() -> None:
     with timer('Data gathering'):
         location_dicts = EVENT_LOOP.run_until_complete(_gather_location_dicts())
-    sanity_check_location_dicts(location_dicts)
     with open(OUTPUT_JSON_FILE, 'w') as json_file_handle:
         json.dump(location_dicts, json_file_handle, indent=4)
+    breakpoint()
     return
 
 if __name__ == '__main__':
