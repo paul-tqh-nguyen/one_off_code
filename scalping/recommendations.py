83c34c421cc90a5922117f663b61b23d128f56c2
Paul Nguyen <pnguyen@0584.attlocal.net>
2020-09-03 10:48:25 -0500

    checkpoint

--- scalping/recommendations.py
+++ scalping/recommendations.py
@@ -30,23 +30,25 @@ import bokeh.models
 RELLEVANT_TICKER_SYMBOLS = [
     'FB',
     'AMZN',
-    'AAPL',
-    'NFLX',
-    'GOOGL',
-    'TSLA',
-    'CMG',
+    # 'AAPL',
+    # 'NFLX',
+    # 'GOOGL',
+    # 'TSLA',
+    # 'CMG',
 ]
 
 NUMBER_OF_DAYS = 5
 
-ENDPOINT_TEMPLATE = 'https://api.tiingo.com/iex/{ticker_symbol}/prices?startDate={start_date_string}&endDate={end_date_string}&resampleFreq=30min'
+ENDPOINT_TEMPLATE = 'https://api.tiingo.com/iex/{ticker_symbol}/prices?startDate={start_date_string}&endDate={end_date_string}&resampleFreq=5min'
 
 TOKEN = os.environ.get('TOKEN')
 assert TOKEN, 'No authentication token specified.'
 
-OUTPUT_DIR = './output'
+DOCS_DIR = './docs'
+
+OUTPUT_DIR = './docs/output'
 if not os.path.isdir(OUTPUT_DIR):
-    os.mkdir(OUTPUT_DIR)
+    os.mkdirs(OUTPUT_DIR)
 
 #############################
 # Domain-Specific Utilities #
@@ -66,13 +68,13 @@ def get_nth_previous_business_day(n: int) -> datetime.date:
 def main() -> None:
     start_date_string = get_nth_previous_business_day(NUMBER_OF_DAYS).strftime("%Y-%m-%d")
     end_date_string = datetime.date.today().strftime("%Y-%m-%d")
-    headers = {'Content-Type': 'application/json', 'Authorization' : f'Token {TOKEN}'}
+    ticker_data = recursive_defaultdict()
     for ticker_symbol in RELLEVANT_TICKER_SYMBOLS:
         endpoint_url = ENDPOINT_TEMPLATE.format(ticker_symbol=ticker_symbol, start_date_string=start_date_string, end_date_string=end_date_string)
-        response = requests.get(endpoint_url, headers=headers)
+        response = requests.get(endpoint_url, headers={'Content-Type': 'application/json', 'Authorization' : f'Token {TOKEN}'})
         assert response.status_code == 200
         df = pd.DataFrame(response.json())
-        df['date'] = pd.to_datetime(df['date'])
+        df['date'] = pd.to_datetime(df['date']).dt.tz_convert('US/Central').dt.tz_localize(None)
         df['truncated_date'] = df['date'].map(datetime.datetime.date)
         df['time_of_day_string'] = df['date'].map(lambda date: date.strftime('%H:%M'))
         for current_date, group in df.groupby('truncated_date'):
@@ -80,33 +82,31 @@ def main() -> None:
                 title=f'{ticker_symbol} Stock Prices for {current_date}',
                 x_axis_type='datetime',
                 y_axis_type='linear',
+                x_axis_label = 'Time',
+                y_axis_label = 'Stock Price',
                 plot_height=400,
                 plot_width=800,
                 x_range=(group.date.min(), group.date.max()),
-                tools='hover',
+                tools='hover, crosshair',
             )
             p.toolbar.logo = None
             p.toolbar_location = None
-            p.xaxis.axis_label = 'Time'
-            p.yaxis.axis_label = 'Stock Price'
-            p.xaxis.ticker = bokeh.models.tickers.DatetimeTicker(desired_num_ticks=16)
             source = bokeh.models.ColumnDataSource(data=group)
             p.line(x='date', y='open', source=source, line_width=3)
             hovertool = p.select_one(bokeh.models.HoverTool)
             hovertool.tooltips = [
-                ('Ticker Symbol', ticker_symbol),
-                ('Date', '@time_of_day_string'),
                 ('Stock Price', '@open{$0,0.00}'),
+                ('Time of Day', '@time_of_day_string'),
+                ('Ticker Symbol', ticker_symbol),
             ]
             p.xaxis.formatter = bokeh.models.DatetimeTickFormatter(
-                hours=['%H:%M'],
+                hours=['%I:%M %p'],
             )
             hovertool.mode = 'vline'
             output_file_location = os.path.join(OUTPUT_DIR, f'{ticker_symbol}_{current_date}_plot.html')
             bokeh.plotting.output_file(output_file_location, title='{ticker_symbol} Stock Prices {current_date}', mode='inline')
             bokeh.plotting.save(p, title='{current_date} {ticker_symbol} Stock Prices')
-            break # @todo remove this
-        break # @todo remove this
+            ticker_data[ticker_symbol][current_date.isoformat()] = {'html_file': os.path.relpath(output_file_location, DOCS_DIR)} # @todo add more data to this dict
     return
 
 if __name__ == '__main__':
