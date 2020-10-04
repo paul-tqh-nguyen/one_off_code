fc992c93f46030af57696b3f56427c17dd4f7bd2
Paul Nguyen <paul.tqh.nguyen@gmail.com>
2020-10-04 08:59:42 -0700

    save

--- graph2vec/main.py
+++ graph2vec/main.py
@@ -139,7 +139,8 @@ class Graph2VecHyperParameterSearchObjective:
         
         graph_embedding_matrix: np.ndarray = np.array([model.docvecs[str(i)] for i in range(len(documents))])
         graph_id_to_graph_embeddings = VectorDict(self.graph_id_to_graph.keys(), graph_embedding_matrix)
-        
+
+        pdir(model)
         loss = model.get_latest_training_loss()
 
         saved_model_location = os.path.join(checkpoint_directory, DOC2VEC_MODEL_FILE_BASENAME)
