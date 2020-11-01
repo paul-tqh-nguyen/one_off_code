d2a724d0be2ee32825b3bae156bf9b6bc5615dc1
Paul Nguyen <paul.tqh.nguyen@gmail.com>
2020-11-01 14:52:48 -0800

    save

--- facebook/link_predictor.py
+++ facebook/link_predictor.py
@@ -224,7 +224,7 @@ class LinkPredictor(pl.LightningModule):
         assert tuple(hadamard_product_batch.shape) == (batch_size, self.hparams.embedding_size), f'{tuple(hadamard_product_batch.shape)} != {(batch_size, self.hparams.embedding_size)}'
 
         prediction_batch = self.logistic_regression_layers(hadamard_product_batch).squeeze()
-        assert len(prediction_batch.shape) == 1, f'{len(prediction_batch.shape)} == 1'
+        assert len(prediction_batch.shape) == 1, f'len({prediction_batch.shape}) == 1'
         assert only_one(prediction_batch.shape) == batch_size, f'{only_one(prediction_batch.shape)} != {batch_size}'
        
         return prediction_batch
