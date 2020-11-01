7f99c493731982285d0d23231c2e8e36e22732f6
Paul Nguyen <paul.tqh.nguyen@gmail.com>
2020-10-31 21:51:31 -0700

    save

--- facebook/link_predictor.py
+++ facebook/link_predictor.py
@@ -462,6 +462,7 @@ class LinkPredictor(pl.LightningModule):
         LOGGER.info(f'Testing AUROC: {testing_auroc}')
 
         with open(os.path.join(checkpoint_dir, RESULT_SUMMARY_JSON_FILE_BASENAME), 'w') as f:
-            json.dump(all_graph_data, f, indent=4)
+            result_summary_dict = {}
+            json.dump(result_summary_dict, f, indent=4)
         
         return best_validation_loss
