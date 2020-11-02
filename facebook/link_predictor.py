6f74f36c1cc32987d3f96e5ed4c1d4bc854b236b
Paul Nguyen <paul.tqh.nguyen@gmail.com>
2020-11-02 08:05:21 -0800

    save

--- facebook/link_predictor.py
+++ facebook/link_predictor.py
@@ -475,6 +475,7 @@ class LinkPredictor(pl.LightningModule):
             result_summary_dict['embedding_visualization_location'] = model.embedding_visualization_location
             result_summary_dict['best_validation_model_path'] = checkpoint_callback.best_model_path
             result_summary_dict['best_validation_loss'] = best_validation_loss
+            len(self.training_dataloader.dataset)
             json.dump(result_summary_dict, f, indent=4)
         
         return best_validation_loss
