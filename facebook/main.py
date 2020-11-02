cc609780c2d3e55c75a7b6372e6218f13cb63e67
Paul Nguyen <paul.tqh.nguyen@gmail.com>
2020-11-02 05:54:13 -0800

    save

--- facebook/main.py
+++ facebook/main.py
@@ -123,6 +123,9 @@ def process_data() -> Tuple[nx.Graph, np.ndarray, np.ndarray]:
 ########################################
 
 class LinkPredictorHyperParameterSearchObjective:
+
+    from link_predictor import LinkPredictor
+    
     def __init__(self, graph: nx.Graph, positive_edges: np.ndarray, negative_edges: np.ndarray, gpu_id_queue: object):
         # gpu_id_queue is an mp.managers.AutoProxy[Queue] and an mp.managers.BaseProxy ; can't declare statically since the classes are generated dynamically
         self.gpu_id_queue = gpu_id_queue
