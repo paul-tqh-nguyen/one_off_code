6c90dac24f593fa403cd2dd8d3ac033d0152c5aa
Paul Nguyen <pnguyen@0584.attlocal.net>
2020-11-08 15:42:50 -0600

    save

--- autograd/autograd/variable.py
+++ autograd/autograd/variable.py
@@ -75,8 +75,7 @@ class Variable:
                 kwargs[internally_used_name] = replaced_callable
                 return func(*args, **kwargs)
             decorated_function.__name__ = func.__name__ # @todo test invariant holds
-            
-            _replace_numpy_method()
+            cls._replace_numpy_method()
             return decorated_function
         return decorator
     
