574fc4bdd2d5c8eb3fce3f0508897f31c6712695
Paul Nguyen <pnguyen@0584.attlocal.net>
2020-11-08 14:57:10 -0600

    save

--- autograd/autograd/tests/test_variable.py
+++ autograd/autograd/tests/test_variable.py
@@ -13,7 +13,7 @@ from autograd import Variable
 
 @contextmanager
 def dummy_numpy_func(dummy_func_name: str) -> Generator:
-    assert not hasattr(np, dummy_func)
+    assert not hasattr(np, dummy_func_name)
     yield
     return
 
@@ -22,7 +22,7 @@ def dummy_numpy_func(dummy_func_name: str) -> Generator:
 #########
 
 def test_differentiable_method():
-    with dummy_numpy_func
+    with dummy_numpy_func('dummy_func') as dummy_func_name:
     def dummy_func(array: np.ndarray) -> np.ndarray:
         return array*10    
     assert dummy_func.__qualname__ == dummy_func
