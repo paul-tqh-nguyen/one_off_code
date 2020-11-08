24ebf482dd88bf5a76d9a924e6490c99929716a2
Paul Nguyen <pnguyen@0584.attlocal.net>
2020-11-08 16:01:48 -0600

    save

--- autograd/autograd/tests/test_variable.py
+++ autograd/autograd/tests/test_variable.py
@@ -66,7 +66,7 @@ def test_numpy_replacement_fails_on_multiple_inputs():
         
         assert np.all(np.mult_ten(np.ones(4)) == np.full([4], 10))
 
-        with pytest.raises(AttributeError, match="does_not_exist"):
+        with pytest.raises(ValueError, match="does_not_exist"):
             @Variable.numpy_replacement(np_mult_ten='np.mult_ten') # @todo test these numpy methods
             def mult_ten(operand: VariableOperand, np_mult_ten: Callable) -> np.ndarray:
                 if isinstance(operand, Variable):
