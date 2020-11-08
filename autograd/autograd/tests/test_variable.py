e678d4a51ba25ae4cfe4511fccb313b117a4da66
Paul Nguyen <pnguyen@0584.attlocal.net>
2020-11-08 15:21:44 -0600

    save

--- autograd/autograd/tests/test_variable.py
+++ autograd/autograd/tests/test_variable.py
@@ -17,8 +17,6 @@ def temp_numpy_func(temp_func: Callable) -> Generator:
     assert not hasattr(np, temp_func.__qualname__)
     setattr(np, temp_func.__qualname__, temp_func)
     assert hasattr(np, temp_func.__qualname__)
-    print(f"temp_func.__qualname__ {repr(temp_func.__qualname__)}")
-    print(f"temp_func.__name__ {repr(temp_func.__name__)}")
     yield
     delattr(np, temp_func.__qualname__)
     assert not hasattr(np, temp_func.__qualname__)
