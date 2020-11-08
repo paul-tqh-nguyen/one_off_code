dd04836fdd77362fa168d179dd5de06b08db1c37
Paul Nguyen <pnguyen@0584.attlocal.net>
2020-11-08 16:22:10 -0600

    save

--- autograd/autograd/tests/test_variable.py
+++ autograd/autograd/tests/test_variable.py
@@ -120,7 +120,7 @@ def test_numpy_replacement_fails_on_bogus_numpy_names():
     
     with temp_numpy_funcs(mult_ten):
         with pytest.raises(ValueError, match='does not specify a numpy function.'):
-            @Variable.numpy_replacement(np_mult_ten='np.bogus\name')
+            @Variable.numpy_replacement(np_mult_ten='np.bogus\name\with\slashes')
             def mult_ten(*args, **kwargs) -> None:
                 pass
             
