6cce81a295e816e129fb7cc9f5c817abbb92915d
Paul Nguyen <pnguyen@0584.attlocal.net>
2020-11-08 15:59:13 -0600

    save

--- autograd/autograd/tests/test_variable.py
+++ autograd/autograd/tests/test_variable.py
@@ -56,3 +56,34 @@ def test_basic_numpy_replacement():
         # Verify 3-D arrays
         var = Variable(np.arange(8).reshape([2,2,2]))
         assert np.all(np.mult_ten(var).data == np.array([[[00, 10], [20, 30]], [[40, 50], [60, 70]]]))
+
+def test_numpy_replacement_fails_on_multiple_replacement_names():
+    def mult_ten(operand: Union[int, float, np.number, np.ndarray]) -> Union[int, float, np.number, np.ndarray]:
+        return operand*10
+    with temp_numpy_func(mult_ten):
+        
+        assert np.all(np.mult_ten(np.ones(4)) == np.full([4], 10))
+        
+        @Variable.numpy_replacement(np_mult_ten='np.mult_ten') # @todo test these numpy methods
+        def mult_ten(operand: VariableOperand, np_mult_ten: Callable) -> np.ndarray:
+            if isinstance(operand, Variable):
+                return Variable(np_mult_ten(operand.data))
+            else:
+                return np_mult_ten(operand)
+
+        assert np.mult_ten.__name__ == 'mult_ten'
+
+        # Verify numpy still works
+        assert np.all(np.mult_ten(np.ones(4)) == np.full([4], 10))
+
+        # Verify 1-D arrays
+        var = Variable(np.arange(3))
+        assert np.all(np.mult_ten(var).data == np.array([00, 10, 20]))
+
+        # Verify 2-D arrays
+        var = Variable(np.arange(4).reshape([2,2]))
+        assert np.all(np.mult_ten(var).data == np.array([[00, 10], [20, 30]]))
+
+        # Verify 3-D arrays
+        var = Variable(np.arange(8).reshape([2,2,2]))
+        assert np.all(np.mult_ten(var).data == np.array([[[00, 10], [20, 30]], [[40, 50], [60, 70]]]))
