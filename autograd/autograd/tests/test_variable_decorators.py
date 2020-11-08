7cb2998266bdce9320e214e593dafd4466e916be
Paul Nguyen <pnguyen@0584.attlocal.net>
2020-11-08 17:19:14 -0600

    save

--- autograd/autograd/tests/test_variable_decorators.py
+++ autograd/autograd/tests/test_variable_decorators.py
@@ -197,7 +197,7 @@ def test_differentiable_method_unary_two_names():
     def mult_ten(operand: Union[int, float, np.number, np.ndarray]) -> Union[int, float, np.number, np.ndarray]:
         return operand*10
     
-    with temp_numpy_funcs(mult_ten):
+    with temp_variable_method_names('mult_ten_special_name'):
         
         assert np.all(np.mult_ten(np.ones(4)) == np.full([4], 10))
         
