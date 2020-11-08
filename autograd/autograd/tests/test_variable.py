4bb0266e1c4db1b1492af8a04daf4ba986206197
Paul Nguyen <pnguyen@0584.attlocal.net>
2020-11-08 15:26:05 -0600

    save

--- autograd/autograd/tests/test_variable.py
+++ autograd/autograd/tests/test_variable.py
@@ -36,7 +36,7 @@ def test_basic_numpy_replacement():
         @Variable.numpy_replacement(np_mult_ten='np.mult_ten') # @todo test these numpy methods
         def mult_ten(array: VariableOperand) -> np.ndarray:
             if isinstance(array, Variable):
-                data = array.data*10
+                data = array.data
                 return Variable()
             return 
 
