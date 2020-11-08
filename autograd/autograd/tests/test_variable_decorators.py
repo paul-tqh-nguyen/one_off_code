a1ac1cc83976521e141beac8eea780f04f331e5d
Paul Nguyen <pnguyen@0584.attlocal.net>
2020-11-08 16:48:19 -0600

    save

--- autograd/autograd/tests/test_variable_decorators.py
+++ autograd/autograd/tests/test_variable_decorators.py
@@ -132,28 +132,28 @@ def test_basic_differentiable_method_unary(): # @todo test binary case as well
     def mult_ten(operand: Union[int, float, np.number, np.ndarray]) -> Union[int, float, np.number, np.ndarray]:
         return operand*10
     
-    # with temp_numpy_funcs(mult_ten):
+    with temp_numpy_funcs(mult_ten):
         
-    #     assert np.all(np.mult_ten(np.ones(4)) == np.full([4], 10))
+        assert np.all(np.mult_ten(np.ones(4)) == np.full([4], 10))
         
-    #     @Variable.differentiable_method()
-    #     def mult_ten(operand: VariableOperand) -> np.ndarray:
-    #         if isinstance(operand, Variable):
-    #             return Variable(np.mult_ten(operand.data))
-    #         else:
-    #             return np.mult_ten(operand)
+        @Variable.differentiable_method()
+        def mult_ten(operand: VariableOperand) -> np.ndarray:
+            if isinstance(operand, Variable):
+                return Variable(np.mult_ten(operand.data))
+            else:
+                return np.mult_ten(operand)
         
-    #     # Verify 1-D arrays
-    #     var = Variable(np.arange(3))
-    #     assert np.all(var.mult_ten().data == np.array([00, 10, 20]))
+        # Verify 1-D arrays
+        var = Variable(np.arange(3))
+        assert np.all(var.mult_ten().data == np.array([00, 10, 20]))
 
-    #     # Verify 2-D arrays
-    #     var = Variable(np.arange(4).reshape([2,2]))
-    #     assert np.all(var.mult_ten().data == np.array([[00, 10], [20, 30]]))
+        # Verify 2-D arrays
+        var = Variable(np.arange(4).reshape([2,2]))
+        assert np.all(var.mult_ten().data == np.array([[00, 10], [20, 30]]))
 
-    #     # Verify 3-D arrays
-    #     var = Variable(np.arange(8).reshape([2,2,2]))
-    #     assert np.all(var.mult_ten().data == np.array([[[00, 10], [20, 30]], [[40, 50], [60, 70]]]))
+        # Verify 3-D arrays
+        var = Variable(np.arange(8).reshape([2,2,2]))
+        assert np.all(var.mult_ten().data == np.array([[[00, 10], [20, 30]], [[40, 50], [60, 70]]]))
     
     with temp_numpy_funcs(mult_ten):
         
