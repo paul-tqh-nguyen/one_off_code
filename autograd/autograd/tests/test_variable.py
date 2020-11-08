11404b2098907550df2dbeb00d33a58be88bba4c
Paul Nguyen <pnguyen@0584.attlocal.net>
2020-11-08 14:51:12 -0600

    save

--- autograd/autograd/tests/test_variable.py
+++ autograd/autograd/tests/test_variable.py
@@ -7,12 +7,12 @@ import autograd
 from autograd import Variable
 
 def test_():
-
     def dummy_func(array: np.ndarray) -> np.ndarray:
         return array*10
+    
+    assert not hasattr(np, dummy_func.__qualname__)
     setattr(np, unique_operation_name, dummy_func)
     assert np.dummy_func(np.
     
     delattr(np, unique_operation_name)
     assert not hasattr(np, dummy_func.__qualname__)
-    
