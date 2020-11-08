032f993f97360373494952a16c53dbc14bebaf99
Paul Nguyen <pnguyen@0584.attlocal.net>
2020-11-08 14:47:16 -0600

    save

--- autograd/autograd/tests/test_variable.py
+++ autograd/autograd/tests/test_variable.py
@@ -1,9 +1,10 @@
 import pytest
 import numpy as np
+from uuid import uuid4
 
 import sys ; sys.path.append("..")
 import autograd
 from autograd import Variable
 
 def test_():
-    
+    numpy_replacement
