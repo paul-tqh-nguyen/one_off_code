23ca9847596a62f82f45fa1287bac4fce9ed31a7
Paul Nguyen <pnguyen@0584.attlocal.net>
2020-11-09 12:06:27 -0600

    Save

--- autograd/autograd/variable.py
+++ autograd/autograd/variable.py
@@ -182,7 +182,7 @@ def all(operand: VariableOperand, np_all: Callable, **kwargs) -> Union[bool, np.
 
 # @todo test this with all combinations of types
 @Variable.new_method('eq', '__eq__') # @todo test this method
-@Variable.numpy_replacement(np_dot='np.dot') # @todo test these numpy methods
+@Variable.numpy_replacement(np_equal='np.equal') # @todo test these numpy methods
 def dot(a: VariableOperand, b: VariableOperand, np_dot: Callable, **kwargs) -> VariableOperand:
     a_is_variable = isinstance(a, Variable)
     b_is_variable = isinstance(b, Variable)
