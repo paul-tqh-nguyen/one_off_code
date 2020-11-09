665c3e2abaf7b490f16f3f85afa7d7decb8fea9d
Paul Nguyen <pnguyen@0584.attlocal.net>
2020-11-09 10:50:29 -0600

    Save

--- autograd/autograd/variable.py
+++ autograd/autograd/variable.py
@@ -157,6 +157,8 @@ class Variable:
      def eq(self, other: VariableOperand) -> Union[bool, np.ndarray]:
          return self == other
 
+     # @todo add gt,gte,le,lte,neq
+
 #######################
 # Variable Operations #
 #######################
