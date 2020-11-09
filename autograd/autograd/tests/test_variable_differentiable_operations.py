611f62c498b866cd73692b8be95b7431f9778b74
Paul Nguyen <pnguyen@0584.attlocal.net>
2020-11-09 15:23:42 -0600

    Save

--- autograd/autograd/tests/test_variable_differentiable_operations.py
+++ autograd/autograd/tests/test_variable_differentiable_operations.py
@@ -5,22 +5,22 @@ import sys ; sys.path.append('..')
 import autograd
 from autograd import Variable
 
-# def test_variable_dot():
-#     a_array = np.arange(5)
-#     b_array = np.array([3, 8, 5, 6, 8])
-#     a = Variable(np.arange(5))
-#     b = Variable(np.array([3, 8, 5, 6, 8]))
-#     expected_result = 68
+def test_variable_dot():
+    a_array = np.arange(5)
+    b_array = np.array([3, 8, 5, 6, 8])
+    a = Variable(np.arange(5))
+    b = Variable(np.array([3, 8, 5, 6, 8]))
+    expected_result = 68
     
-#     assert np.all(a_array == a.data)
-#     assert np.all(b_array == b.data)
-#     assert id(a_array) != id(a.data)
-#     assert id(b_array) != id(b.data)
+    assert np.all(a_array == a.data)
+    assert np.all(b_array == b.data)
+    assert id(a_array) != id(a.data)
+    assert id(b_array) != id(b.data)
     
-#     # Variable + Variable
-#     print(f"a.dot(b).data {repr(a.dot(b).data)}")
-#     assert a.dot(b) == expected_result
-#     assert np.dot(a, b) == expected_result
+    # Variable + Variable
+    print(f"a.dot(b).data {repr(a.dot(b).data)}")
+    assert a.dot(b) == expected_result
+    assert np.dot(a, b) == expected_result
     
 #     # nupmy + numpy
 #     assert np.dot(a_array, b_array) == expected_result
