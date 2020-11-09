e5259d8f43cbbb038e3d3057fba81ee831bddff3
Paul Nguyen <pnguyen@0584.attlocal.net>
2020-11-09 13:34:35 -0600

    Save

--- autograd/autograd/variable.py
+++ autograd/autograd/variable.py
@@ -38,6 +38,9 @@ class Variable:
             raise ValueError(f'Only one numpy callable can be replaced. {len(internally_used_name_to_np_paths)} were specified.')
         
         internally_used_name, np_paths = only_one(internally_used_name_to_np_paths.items())
+
+        if not len(np_path) > 0: # @todo test this
+            
         
         if not internally_used_name.isidentifier():
             raise ValueError(f'"{internally_used_name}" is not a vaild identifier name.')
