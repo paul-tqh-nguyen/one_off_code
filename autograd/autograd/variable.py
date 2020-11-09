b249e2a9cac56fdcf213e9c1bccc0d90f37e2880
Paul Nguyen <pnguyen@0584.attlocal.net>
2020-11-08 19:10:24 -0600

    save

--- autograd/autograd/variable.py
+++ autograd/autograd/variable.py
@@ -117,8 +117,8 @@ class Variable:
         '''
         self.data = data
         self.directly_depended_on_variable_to_backward_propagation_function = directly_depended_on_variable_to_backward_propagation_function
-        self.counter += 1
-        self.id = self.counter
+        self.__class__.counter += 1
+        self.id = self.__class__.counter
         return
 
     def __repr__(self):
