b10906d1850bf453e0493c4134c9fdcb101941d6
Paul Nguyen <pnguyen@0584.attlocal.net>
2020-11-09 14:26:42 -0600

    Save

--- autograd/autograd/variable.py
+++ autograd/autograd/variable.py
@@ -89,7 +89,7 @@ class Variable:
     
     @classmethod
     def numpy_replacement(cls, **internally_used_name_to_np_path_specification: Dict[str, Union[List[str], str]]) -> Callable:
-        '''Replaces numpy methods via monkey patching.'''
+        '''Replaces numpy methods via monkey patching. The repalced'''
         internally_used_name, np_paths, replaced_callables = cls._numpy_replacement_extract_inputs(internally_used_name_to_np_path_specification)
         def decorator(func: Callable) -> Callable:
             for np_path, replaced_callable in zip(np_paths, replaced_callables):
