ebab7c03dd37c08958504dc21540b5a2283ad2b9
Paul Nguyen <pnguyen@0584.attlocal.net>
2020-11-09 14:45:16 -0600

    Save

--- autograd/autograd/variable.py
+++ autograd/autograd/variable.py
@@ -85,6 +85,9 @@ class Variable:
         module = np
         for np_path_sub_attribute in np_path_sub_attributes[1:-1]:
             module = getattr(module, np_path_sub_attribute)
+        module
+        np_path_sub_attributes[-1]
+        replacement_function
         forbiddenfruit.curse(module, np_path_sub_attributes[-1], replacement_function)
         return
     
