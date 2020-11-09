e44e1947e42c0c2e4d3eef2e37b4454ab75211c6
Paul Nguyen <pnguyen@0584.attlocal.net>
2020-11-09 11:03:43 -0600

    Save

--- autograd/autograd/variable.py
+++ autograd/autograd/variable.py
@@ -161,8 +161,7 @@ class Variable:
 ##########################################
 
 @Variable.new_method() # @todo test this method
-@Variable.numpy_replacement(np_dot='np.dot') # @todo test these numpy methods
-def dot(a: VariableOperand, b: VariableOperand, np_dot: Callable, **kwargs) -> VariableOperand:
+def eq(a: VariableOperand, b: VariableOperand) -> VariableOperand:
     a_is_variable = isinstance(a, Variable)
     b_is_variable = isinstance(b, Variable)
     a_data = a.data if a_is_variable else a
