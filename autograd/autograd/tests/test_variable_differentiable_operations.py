070ac0677dc0be0d00431dfabaa3b761d8443c8e
Paul Nguyen <pnguyen@0584.attlocal.net>
2020-11-09 19:04:13 -0600

    Save

--- autograd/autograd/tests/test_variable_differentiable_operations.py
+++ autograd/autograd/tests/test_variable_differentiable_operations.py
@@ -19,10 +19,6 @@ def test_variable_dot():
 
     def validate_result(result, expected_type: type) -> None:
         assert result == expected_result
-        print()
-        print(f"result {repr(result)}")
-        print(f"type(result) {repr(type(result))}")
-        print(f"expected_type {repr(expected_type)}")
         assert isinstance(result, expected_type)
         return
     
