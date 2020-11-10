6a0f27bb10a103ac4002abd74ca79132d3d51203
Paul Nguyen <pnguyen@0584.attlocal.net>
2020-11-09 19:08:02 -0600

    Save

--- autograd/autograd/tests/test_variable_differentiable_operations.py
+++ autograd/autograd/tests/test_variable_differentiable_operations.py
@@ -38,5 +38,40 @@ def test_variable_dot():
     # numpy + Variable
     validate_result(np.dot(a, b), Variable)
     validate_result(np.ndarray.dot(a, b), Variable)
+    validate_result(a_array.dot(b), Variable)    
+
+def test_variable_dot():
+    a_array = np.arange(5)
+    b_array = np.array([3, 8, 5, 6, 8])
+    a = Variable(np.arange(5))
+    b = Variable(np.array([3, 8, 5, 6, 8]))
+    expected_result = 68
+    
+    assert np.all(a_array == a.data)
+    assert np.all(b_array == b.data)
+    assert id(a_array) != id(a.data)
+    assert id(b_array) != id(b.data)
+
+    def validate_result(result, expected_type: type) -> None:
+        assert result == expected_result
+        assert isinstance(result, expected_type)
+        return
+    
+    # Variable + Variable
+    validate_result(a.dot(b), Variable)
+    validate_result(np.dot(a, b), Variable)
+    
+    # nupmy + numpy
+    validate_result(np.dot(a_array, b_array), np.int64)
+    validate_result(np.ndarray.dot(a_array, b_array), np.int64)
+    validate_result(a_array.dot(b_array), np.int64)
+    
+    # Variable + numpy
+    validate_result(a.dot(b_array), Variable)
+    validate_result(np.dot(a, b_array), Variable)
+    
+    # numpy + Variable
+    validate_result(np.dot(a, b), Variable)
+    validate_result(np.ndarray.dot(a, b), Variable)
     validate_result(a_array.dot(b), Variable)
     
