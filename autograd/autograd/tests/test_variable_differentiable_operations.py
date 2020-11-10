407085a7d489ebd1d13404bb042ada0bdd063b6f
Paul Nguyen <pnguyen@0584.attlocal.net>
2020-11-09 19:09:06 -0600

    Save

--- autograd/autograd/tests/test_variable_differentiable_operations.py
+++ autograd/autograd/tests/test_variable_differentiable_operations.py
@@ -45,7 +45,7 @@ def test_variable_subtract():
     b_array = np.array([3, 8, 5, 6, 8])
     a = Variable(np.arange(5))
     b = Variable(np.array([3, 8, 5, 6, 8]))
-    expected_result = 68
+    expected_result_array = 68
     
     assert np.all(a_array == a.data)
     assert np.all(b_array == b.data)
