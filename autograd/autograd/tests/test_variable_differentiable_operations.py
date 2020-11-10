70bd31a46a819c0b86844033db73f545a07b138e
Paul Nguyen <pnguyen@0584.attlocal.net>
2020-11-09 19:17:04 -0600

    Save

--- autograd/autograd/tests/test_variable_differentiable_operations.py
+++ autograd/autograd/tests/test_variable_differentiable_operations.py
@@ -81,6 +81,6 @@ def test_variable_subtract():
     validate_variable_result(np.subtract(a, b_array))
     
     # numpy + Variable
-    validate_variable_result(np.subtract(a, b))
-    validate_variable_result(a - b)
+    validate_variable_result(np.subtract(a_array, b))
+    validate_variable_result(a_array - b)
     
