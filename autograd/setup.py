d92a01c1051102bc253bbf4d4c56a43eddc5bcb0
Paul Nguyen <pnguyen@0584.attlocal.net>
2020-11-10 23:55:08 -0600

    save

--- autograd/setup.py
+++ autograd/setup.py
@@ -1,17 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="autograd",
-    description="Automatic Differentiation Engine",
-    author="Paul Nguyen",
-    author_email="paul.tqh.nguyen@gmail.com",
-    packages=find_packages(include=["metagraph", "metagraph.*"]),
-    python_requires=">=3.7",
-    install_requires=["numpy", "more-itertools", "forbiddenfruit"],
-    classifiers=[
-        "License :: OSI Approved :: Apache Software License",
-        "Programming Language :: Python :: 3 :: Only",
-        "Topic :: Scientific/Engineering :: Information Analysis",
-        "Topic :: Scientific/Engineering :: Mathematics",
-    ],
+    name='autograd',
+    description='Automatic Differentiation Engine',
+    author='Paul Nguyen',
+    author_email='paul.tqh.nguyen@gmail.com',
+    version='0.0.1',
+    packages=find_packages(include=['autograd']),
+    python_requires='>=3.7',
+    install_requires=['numpy', 'more-itertools', 'forbiddenfruit'],
+    classifiers=['Programming Language :: Python :: 3 :: Only'],
+    # url='' # @todo fill this in
 )
