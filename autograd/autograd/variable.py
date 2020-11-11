6bd499eda05742f84e35dd913fde4f93ac486f99
Paul Nguyen <pnguyen@0584.attlocal.net>
2020-11-11 02:18:19 -0600

    save

--- autograd/autograd/variable.py
+++ autograd/autograd/variable.py
@@ -194,11 +194,11 @@ class Variable:
     
     def sum(self, **kwargs) -> 'Variable':
         summation = self.data.sum(**kwargs)
+        if 'axis' in kwargs:
+            del kwargs['axis']
+        if 'out' in kwargs:
+            del kwargs['out']
         if len(kwargs) > 0:
-            if 'axis' in kwargs:
-                del kwargs['axis']
-            print(f"kwargs['axis'] {repr(kwargs['axis'])}")
-            print(f"kwargs['out'] {repr(kwargs['out'])}")
             raise ValueError(f'The parameters {[repr(kwarg_name) for kwarg_name in kwargs.keys()]} are not supported for {Variable.__qualname__}.')
         variable_depended_on_by_summation_to_backward_propagation_functions = defaultdict(list)
         variable_depended_on_by_summation_to_backward_propagation_functions[self].append(lambda d_minimization_target_over_d_summation: d_minimization_target_over_d_summation * np.ones(self.shape))
@@ -392,7 +392,7 @@ def sum(operand: VariableOperand, np_sum: Callable, **kwargs) -> VariableOperand
     operand_is_variable = isinstance(operand, Variable)
     operand_data = operand.data if operand_is_variable else operand
     summation = np_sum(operand, **kwargs)
-    if not a_is_variable:
+    if not operand_is_variable:
         return summation
     if len(kwargs) > 0:
         raise ValueError(f'The parameters {[repr(kwarg_name) for kwarg_name in kwargs.keys()]} are not supported for {Variable.__qualname__}.')
