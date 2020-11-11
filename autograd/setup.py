1f6adb98a243f4c76408d0fa1debffde1352973d
Paul Nguyen <pnguyen@0584.attlocal.net>
2020-11-10 23:34:55 -0600

    save

---
+++ autograd/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="autograd",
+    description="",
+    author="Paul Nguyen",
+    author_email="paul.tqh.nguyen@gmail.com",
+    packages=find_packages(include=["metagraph", "metagraph.*"]),
+    python_requires=">=3.7",
+    install_requires=[
+        "importlib_metadata",
+        "numpy",
+        "scipy",
+        "donfig",
+        "networkx",
+        "pandas",
+        "python-louvain",
+        "nest-asyncio",
+        "dask[array,dataframe,delayed]",
+        "graphviz",
+    ],
+    include_package_data=True,
+    entry_points={"metagraph.plugins": ["plugins=metagraph.plugins:find_plugins",]},
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python :: 3 :: Only",
+        "Topic :: Scientific/Engineering :: Information Analysis",
+        "Topic :: Scientific/Engineering :: Mathematics",
+    ],
+)
