b1951499aed1a48add6f84bfd275dfd3a92aa610
Paul Nguyen <pnguyen@0584.attlocal.net>
2020-11-30 20:31:04 -0600

    Save

--- mlir/main.py
+++ mlir/main.py
@@ -8,11 +8,8 @@
 # Imports #
 ###########
 
+import numpy as np
 import tensorflow as tf
-
-from tensorflow import keras
-from tensorflow.keras import layers
-from tensorflow.keras.layers.experimental import preprocessing
 
 # @todo verify that these imports are used
 
@@ -20,8 +17,32 @@ from tensorflow.keras.layers.experimental import preprocessing
 # Driver #
 ##########
 
+def example_1():    
+    model = tf.keras.models.Sequential([
+        tf.keras.Input(shape=(2,)),
+        tf.keras.layers.Dense(units=1)
+    ])
+    
+    model.compile(loss='mse', optimizer='adam', metrics=['accuracy'])
+
+    x = np.arange(2, dtype=float).reshape([1,2])
+    
+    print(f"model.predict(x) {repr(model.predict(x))}")
+    print(f"model.summary() {repr(model.summary())}")
+
+
 if __name__ == '__main__':
+    print('\n'*80)
     
-    model = layers.Dense(units=1)
+    g = tf.Graph()
+    with g.as_default():
+        inputs = tf.keras.Input(shape=(2,)) 
+        x = ttf.keras.layers.Dense(units=1)(inputs)
+        
+        ops = g.get_operations()
+        for op in ops:
+            print(op.name, op.type)
+            
+        tf.io.write_graph(g.as_graph_def(), '/tmp/', 'tf_graph.pb', as_text=False)
     
-    model.summary()
+    
