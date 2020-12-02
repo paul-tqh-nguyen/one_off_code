49a9f4e7fc4a70996283ed7abca5d789327d77b1
Paul Nguyen <pnguyen@0584.attlocal.net>
2020-12-01 20:16:45 -0600

    save

--- mlir/main.py
+++ mlir/main.py
@@ -17,27 +17,12 @@ import tensorflow as tf
 # Driver #
 ##########
 
-def example_1():    
-    model = tf.keras.models.Sequential([
-        tf.keras.Input(shape=(2,)),
-        tf.keras.layers.Dense(units=1)
-    ])
-    
-    model.compile(loss='mse', optimizer='adam', metrics=['accuracy'])
-
-    x = np.arange(2, dtype=float).reshape([1,2])
-    
-    print(f"model.predict(x) {repr(model.predict(x))}")
-    print(f"model.summary() {repr(model.summary())}")
-
-
 if __name__ == '__main__':
-    print('\n'*80)
-    
     g = tf.Graph()
     with g.as_default():
         inputs = tf.keras.Input(shape=(2,)) 
         x = tf.keras.layers.Dense(units=1)(inputs)
     graph_def = g.as_graph_def()
     mlir_text = tf.mlir.experimental.convert_graph_def(graph_def, pass_pipeline='tf-standard-pipeline')
-    print(f"mlir_text \n{mlir_text}")
+    print('\n'*80)
+    print(f"mlir_text {mlir_text}")
