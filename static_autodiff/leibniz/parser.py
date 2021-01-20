aba9d4c38d171c6647127c9c09c5a161e8a2196e
Paul Nguyen <pnguyen@0584.attlocal.net>
2021-01-19 21:00:59 -0600

    save

--- static_autodiff/leibniz/parser.py
+++ static_autodiff/leibniz/parser.py
@@ -10,9 +10,10 @@
 
 import pyparsing
 from pyparsing import pyparsing_common as ppc
-from pyparsing import Word
+from pyparsing import Word, Literal
+from pyparsing import oneOf, infixNotation, opAssoc
 
-from misc_utilities import *
+from .misc_utilities import *
 
 # TODO make usure imports are used
 
@@ -26,17 +27,74 @@ from misc_utilities import *
 # Grammar #
 ###########
 
-# ParserElement.setDefaultWhitespaceChars(' \t') # TODO enable this
-
 # Convention: The "_pe" suffix indicates an instance of pyparsing.ParserElement
 
-integer_pe = ppc.signed_integer
+# ParserElement.setDefaultWhitespaceChars(' \t') # TODO enable this
 
+# Atom Parser Elements
+
+def parse_boolean(_s: str, _loc: int, tokens: pyparsing.ParseResults):
+    assert len(tokens) == 1
+    token = tokens.pop()
+    assert token in ('True', 'False')
+    bool_token = True if token == 'True' else False
+    tokens.append(bool_token)
+
+boolean_pe = oneOf('True False').setName('boolean').setParseAction(parse_boolean)
+
+integer_pe = ppc.signed_integer.setName('integer')
 _float_pe = ppc.real | ppc.sci_real
+real_pe = (_float_pe | integer_pe).setName('real number')
 
-real_pe = _float_pe | integer_pe
+identifier_pe = ppc.identifier.setName('identifier')
 
-grammar_pe = real_pe # TODO update this
+atom_pe = (boolean_pe | real_pe | identifier_pe).setName('atom')
+
+# Arithmetic Operation Parser Elements
+
+exponent_operation_pe = oneOf('^ **').setName('power operation')
+multiplication_operation_pe = Literal('*').setName('multiplication operation')
+division_operation_pe = Literal('/').setName('division operation')
+addition_operation_pe = Literal('+').setName('addition operation')
+subtraction_operation_pe = Literal('-').setName('subtraction operation')
+
+# Boolean Operation Parser Elements
+
+not_operation_pe = Literal('not').setName('not operation')
+and_operation_pe = Literal('and').setName('and operation')
+xor_operation_pe = Literal('xor').setName('xor operation')
+or_operation_pe = Literal('or').setName('or operation')
+
+# Expression Parser Elements
+
+arithmetic_expression_pe = infixNotation(
+    real_pe | identifier_pe,
+    [
+        (exponent_operation_pe, 2, opAssoc.RIGHT),
+        (multiplication_operation_pe | division_operation_pe, 2, opAssoc.LEFT),
+        (addition_operation_pe | subtraction_operation_pe, 2, opAssoc.LEFT),
+    ],
+).setName('arithmetic expression')
+
+boolean_expression_pe = infixNotation(
+    boolean_pe | identifier_pe,
+    [
+        (not_operation_pe, 2, opAssoc.RIGHT),
+        (and_operation_pe, 2, opAssoc.LEFT),
+        (xor_operation_pe, 2, opAssoc.LEFT),
+        (or_operation_pe, 2, opAssoc.LEFT),
+    ],
+).setName('boolean expression')
+
+expression_pe = (atom_pe | arithmetic_expression_pe | boolean_expression_pe).setName('expression')
+
+# Declaration Parser Elements
+
+declaration_pe = (identifier_pe + "=" + expression_pe).setName('declaration')
+
+# Grammar
+
+grammar_pe = declaration_pe # TODO update this
 
 ################
 # Entry Points #
--- static_autodiff/leibniz/tests/test_parser.py
+++ static_autodiff/leibniz/tests/test_parser.py
@@ -1,19 +1,53 @@
 
+import pytest
+
 from leibniz import parser
 
-# @debug_on_error
-# def main() -> None:
-#     input_string = '''
-# 0.3213 1.2 -123 1e2 3
-
-def test_parser():
+def test_parser_boolean():
     expected_input_output_pairs = [
+        ('True', True),
+        ('False', False),
+    ]
+    for input_string, expected_result in expected_input_output_pairs:
+        result = parser.parseSourceCode('x = '+input_string).asList()[2]
+        assert result == expected_result
+
+def test_parser_real():
+    expected_input_output_pairs = [ # TODO Make all these cases work
         ('123', 123),
         ('0.123', 0.123),
         ('-0.123', -0.123),
+        ('.123', 0.123),
+        ('-.123', -0.123),
+        # ('1.e2', 100.0),
+        # ('1.E2', 100.0),
+        # ('1.0e2', 100.0),
         ('1e2', 100.0),
-        ('-1e2', -100.0),
+        ('-1E2', -100.0),
+        # ('.23E2', 23.0),
+        # ('-1.23e-2', -0.0123),
     ]
     for input_string, expected_result in expected_input_output_pairs:
-        result = parser.parseSourceCode(input_string).asList()[0]
-        assert result is expected_result
+        result = parser.parseSourceCode('x = '+input_string).asList()[2]
+        assert result == expected_result
+
+def test_parser_identifier():
+    valid_identifiers = [
+        'var',
+        'var_1',
+        '_var',
+        '_var_',
+        '_var_1',
+        'var_1_',
+        '_var_1_',
+        '_12345_',
+        'VAR',
+        'Var',
+        'vAr213feEF',
+    ]
+    for input_string in valid_identifiers:
+        result = parser.parseSourceCode(input_string+' = 1').asList()[0]
+        print(f"result {repr(result)}")
+        assert type(result) is str
+        assert result == input_string.split(' ')[0]
+
