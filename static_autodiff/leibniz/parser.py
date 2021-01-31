638ca928e98ce636b9ff946adf7552bf554b7bfe
Paul Nguyen <pnguyen@0584.attlocal.net>
2021-01-31 14:26:00 -0600

    save

--- static_autodiff/leibniz/parser.py
+++ static_autodiff/leibniz/parser.py
@@ -375,7 +375,17 @@ class LessThanOrEqualToExpressionASTNode(ComparisonExpressionASTNode):
 class EqualToExpressionASTNode(ComparisonExpressionASTNode):
     pass
 
-class NotEqualToExpressionASTNode(ComparisonExpressionASTNode):
+class NotEqualToExpressionASTNode(ComparisonExpressionASTNode
+from typing import Tuple
+def child_classes(obj) -> Tuple[type, ...]:
+    import inspect
+    cls = obj if inspect.isclass(obj) else type(obj)
+    def _child_classes(cls):
+        for subclass in cls.__subclasses__():
+            yield from get_subclasses(subclass)
+            yield subclass
+    return tuple(_child_classes(cls))
+):
     pass
 
 comparator_to_ast_node_class = {
