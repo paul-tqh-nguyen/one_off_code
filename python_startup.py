745b304cd571a36a901b38af41e69d7ced16e50c
Paul Nguyen <pnguyen@0584.attlocal.net>
2021-01-31 14:25:03 -0600

    save

--- python_startup.py
+++ python_startup.py
@@ -183,6 +183,16 @@ def parent_classes(obj) -> Tuple[type, ...]:
     cls = obj if inspect.isclass(obj) else type(obj)
     return inspect.getmro(cls)
 
+from typing import Tuple
+def child_classes(obj) -> Tuple[type, ...]:
+    import inspect
+    cls = obj if inspect.isclass(obj) else type(obj)
+    def _child_classes(cls):
+        for subclass in cls.__subclasses__():
+            yield from get_subclasses(subclass)
+            yield subclass
+    return tuple(cls)
+
 from typing import Iterable
 def p1(iterable: Iterable) -> None:
     for e in iterable:
