ab869c5357bacf6c5dea66da78ad692840927640
Paul Nguyen <pnguyen@0584.attlocal.net>
2021-01-31 15:06:46 -0600

    save

--- static_autodiff/leibniz/parser.py
+++ static_autodiff/leibniz/parser.py
@@ -1041,6 +1041,7 @@ def sanity_check_concrete_ast_node_subclass_method_annotations() -> None:
                     assert return_annotation == 'BinaryOperationExpressionASTNode'
             else:
                 assert return_annotation == ast_node_class.__qualname__, f'{ast_node_class.__qualname__}.parse_action is not declared to return a {ast_node_class.__qualname__}'
+    return
 
 @lru_cache(maxsize=128)
 def perform_sanity_check() -> None:
