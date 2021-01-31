02cf4413fd3213f5fa5959990c9365a80f1600b1
Paul Nguyen <pnguyen@0584.attlocal.net>
2021-01-31 14:38:02 -0600

    save

--- static_autodiff/leibniz/parser.py
+++ static_autodiff/leibniz/parser.py
@@ -1014,6 +1014,7 @@ def parseSourceCode(input_string: str) -> ModuleASTNode:
 ###################
 
 sanity_check_base_types()
+
 for ast_node_class in child_classes(ASTNode):
     parse_action_method = ast_node_class.parse_action
     if not parse_action_method.__isabstractmethod__:
