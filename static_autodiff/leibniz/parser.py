f4ded8c391a3eb310f4a2433ce61f2782b2363a8
Paul Nguyen <pnguyen@0584.attlocal.net>
2021-01-31 15:48:03 -0600

    save

--- static_autodiff/leibniz/parser.py
+++ static_autodiff/leibniz/parser.py
@@ -512,7 +512,7 @@ class AssignmentASTNode(StatementASTNode):
     @classmethod
     def parse_action(cls, _s: str, _loc: int, tokens: pyparsing.ParseResults) -> 'AssignmentASTNode':
         assert len(tokens) is 2
-        variable_declarations: typing.List[typing.Tuple[VariableASTNode, TensorTypeASTNode]] = eager_map(tuple, map(pyparsing.ParseResults.asList, tokens[0]))
+        variable_type_pairs: typing.List[typing.Tuple[VariableASTNode, TensorTypeASTNode]] = eager_map(tuple, map(pyparsing.ParseResults.asList, tokens[0]))
         node_instance = cls(tokens[0], tokens[1], tokens[2])
         return node_instance
 
