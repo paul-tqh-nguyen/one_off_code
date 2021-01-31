9d6a88507934ce71f3112ad010d5994365b15e6f
Paul Nguyen <pnguyen@0584.attlocal.net>
2021-01-31 16:41:35 -0600

    save

--- static_autodiff/leibniz/tests/test_parser.py
+++ static_autodiff/leibniz/tests/test_parser.py
@@ -668,7 +668,7 @@ def test_parser_assignment():
     expected_input_output_pairs = [
         ('x = 1', AssignmentASTNode(variable_type_pairs=[(VariableASTNode(name='x'), TensorTypeASTNode(base_type_name=None, shape=None))], value=IntegerLiteralASTNode(value=1))),
         ('x: NothingType = Nothing', AssignmentASTNode(variable_type_pairs=[(VariableASTNode(name='x'), TensorTypeASTNode(base_type_name='NothingType', shape=[]))], value=NothingTypeLiteralASTNode())),
-        ('x = Nothing', AssignmentASTNode(variable_type_pairs=[(VariableASTNode(name='x'), TensorTypeASTNode(base_type_name=None, shape=None))], identifier_type=TensorTypeASTNode(base_type_name=None, shape=None), value=NothingTypeLiteralASTNode())),
+        ('x = Nothing', AssignmentASTNode(variable_type_pairs=[(VariableASTNode(name='x'), TensorTypeASTNode(base_type_name=None, shape=None))], value=NothingTypeLiteralASTNode())),
         ('x: NothingType<> = Nothing', AssignmentASTNode(variable_type_pairs=[(VariableASTNode(name='x'), TensorTypeASTNode(base_type_name=None, shape=None))], identifier_type=TensorTypeASTNode(base_type_name='NothingType', shape=[]), value=NothingTypeLiteralASTNode())),
         ('x: NothingType<1,?> = Nothing', AssignmentASTNode(
             identifier=VariableASTNode(name='x'),
