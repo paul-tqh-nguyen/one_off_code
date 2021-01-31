b517e9bd651a1c0166056c58713743ead01c549f
Paul Nguyen <pnguyen@0584.attlocal.net>
2021-01-31 14:50:04 -0600

    save

--- static_autodiff/leibniz/parser.py
+++ static_autodiff/leibniz/parser.py
@@ -1021,7 +1021,7 @@ for ast_node_class in child_classes(ASTNode):
         return_annotation = inspect.signature(parse_action_method).return_annotation
         if issubclass(ast_node_class, ComparisonExpressionASTNode):
             assert return_annotation == 'ComparisonExpressionASTNode'
-        elif issubclass(ast_node_class, ArithmeticExpressionASTNode) and issubclass(ast_node_class, (UnaryOperationExpressionASTNode, BinaryOperationExpressionASTNode)):
+        elif issubclass(ast_node_class, ArithmeticExpressionASTNode) and issubclass(ast_node_class, UnaryOperationExpressionASTNode):
             assert return_annotation in ('UnaryOperationExpressionASTNode', 'BinaryOperationExpressionASTNode')
         else:
             assert return_annotation == ast_node_class.__qualname__, f'{ast_node_class.__qualname__}.parse_action is not declared to return a {ast_node_class.__qualname__}'
