49b27f6e12234688b138e918501cf3a3618eeb39
Paul Nguyen <pnguyen@0584.attlocal.net>
2021-01-31 15:04:02 -0600

    save

--- static_autodiff/leibniz/parser.py
+++ static_autodiff/leibniz/parser.py
@@ -1021,6 +1021,12 @@ def sanity_check_concrete_ast_node_subclass_method_annotations() -> None:
         if not getattr(init_method, '__isabstractmethod__', False):
             if not getattr(init_method, '__isabstractmethod__', False):
                 assert inspect.signature(parse_action_method).return_annotation is None
+
+        # __eq__ method
+        init_method = ast_node_class.__init__
+        if not getattr(init_method, '__isabstractmethod__', False):
+            if not getattr(init_method, '__isabstractmethod__', False):
+                assert inspect.signature(parse_action_method).return_annotation is None
         
         # parse_action method
         parse_action_method = ast_node_class.parse_action
