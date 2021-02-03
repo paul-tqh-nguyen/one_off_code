c1ea608d8846fe385e29ee5d303836d2f87979e7
Paul Nguyen <pnguyen@0584.attlocal.net>
2021-02-02 23:04:10 -0600

    save

--- google_stock_scraper/main.py
+++ google_stock_scraper/main.py
@@ -160,9 +160,7 @@ async def gather_ticker_symbol_rows(ticker_symbol: str) -> List[Tuple[datetime.d
         if len(chart_svgs) == 0:
             print(f'SVG not found for {ticker_symbol}')
             return rows
-        elif len(chart_svgs) > 1:
-            print(f'Too many AVGs found.')
-            return rows
+        assert len(chart_svgs) > 1, f'{ticker_symbol} has an unexpected number of SVGs within the chart.'
         
         chart_div = await search_div.get_sole_element('div[jscontroller].knowledge-finance-wholepage-chart__fw-uch')
         top, left, width, height = await page.evaluate('''
