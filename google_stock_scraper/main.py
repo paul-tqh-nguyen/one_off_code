ad32f9a42f5f73c5ae0b1a26f06b28c2f14bc4c8
Paul Nguyen <pnguyen@0584.attlocal.net>
2021-02-02 23:45:02 -0600

    save

--- google_stock_scraper/main.py
+++ google_stock_scraper/main.py
@@ -212,6 +212,7 @@ return [top, left, width, height];
 async def update_stock_db(cursor: sqlite3.Cursor) -> None:    
     semaphore = asyncio.Semaphore(MAX_NUMBER_OF_CONCURRENT_BROWSERS)
     ticker_symbols = await gather_ticker_symbols()
+    print(f'{len(ticker_symbols)} ticker symbols gathered.')
     ticker_symbols = ['CDZI']
     async def semaphore_task(task: Awaitable):
         async with semaphore:
