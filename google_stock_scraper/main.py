165557c16f153c503321582c3703b83bb1c402ab
Paul Nguyen <pnguyen@0584.attlocal.net>
2021-02-02 23:36:30 -0600

    save

--- google_stock_scraper/main.py
+++ google_stock_scraper/main.py
@@ -176,7 +176,8 @@ return [top, left, width, height];
             time_span = await info_card.get_sole_element('span.knowledge-finance-wholepage-chart__hover-card-time')
             whole_time_string = await page.evaluate('(element) => element.innerHTML', time_span)
             if whole_time_string not in seen_whole_time_strings:
-                
+
+                print(f"whole_time_string {repr(whole_time_string)}")
                 time_string, period = whole_time_string.split(' ')
                 hour, minute = eager_map(int, time_string.split(':'))
                 assert period in ('AM', 'PM')
