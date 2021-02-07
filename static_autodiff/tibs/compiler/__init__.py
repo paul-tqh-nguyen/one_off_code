c4b7f7006becba6295d0f74fbaad07ab3a6d3ec5
Paul Nguyen <paul.tqh.nguyen@gmail.com>
2021-02-07 11:23:41 -0800

    save

--- static_autodiff/tibs/compiler/__init__.py
+++ static_autodiff/tibs/compiler/__init__.py
@@ -1,13 +1,64 @@
 
+'''
+'''
+
+# TODO fill in doc string
+
+###########
+# Imports #
+###########
+
 import ctypes
 
-lib = ctypes.CDLL('../build/tibs-main/libtibs-shared.so')
+# TODO make sure imports are used
+# TODO make sure these imports are ordered in some way
 
-print(f"dir(lib) {repr(dir(lib))}")
+###########
+# Globals #
+###########
 
-lib.runAllPasses.restype = None
-lib.runAllPasses.argtypes = []
+LIBTIBS_SO = ctypes.CDLL('../build/tibs-main/libtibs.so')
 
-result = lib.runAllPasses()
+# TODO automatically download the LLVM-project REPO if the directory doesn't exist
+# TODO if it does exist, just do a pull
 
-print('Done.')
+# TOODO set these environment variables
+# export PATH=/home/pnguyen/code/one_off_code/mlir/llvm-project/build/bin/:$PATH
+# export LD_LIBRARY_PATH=/home/pnguyen/miniconda3/envs/mlir/lib:$LD_LIBRARY_PATH
+# export BUILD_DIR=/home/pnguyen/code/one_off_code/mlir/llvm-project/build
+# export PREFIX=/home/pnguyen/code/one_off_code/mlir/llvm-project/build
+
+# TODO do a compile of our MLIR compiler to make sure it is up-to-date (redundant compiles will be fast)
+# cd /home/pnguyen/code/one_off_code/mlir/tibs/
+# mkdir build
+# cd build
+# cmake -G Ninja .. -DMLIR_DIR=$PREFIX/lib/cmake/mlir -DLLVM_EXTERNAL_LIT=$BUILD_DIR/bin/llvm-lit
+# ninja
+
+
+#################################
+# C++ Method Type Declarations  #
+#################################
+
+# TODO we must declare types for all methods
+LIBTIBS_SO.runAllPasses.restype = None
+LIBTIBS_SO.runAllPasses.argtypes = []
+
+############
+# Compiler #
+############
+
+def compile():
+    return LIBTIBS_SO.runAllPasses()
+
+# TODO enable this
+# __all__ = [
+#     'TODO put something here'
+# ]
+
+##########
+# Driver #
+##########
+
+if __name__ == '__main__':
+    print("TODO add something here")
---
+++ static_autodiff/tibs/tests/test_compiler.py
@@ -0,0 +1,10 @@
+
+import pytest
+
+from tibs import compiler
+
+from tibs.misc_utilities import *
+
+def test_compiler():
+    compiler.compile()
+    assert False
