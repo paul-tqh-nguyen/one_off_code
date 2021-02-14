e2ad1b71e838a814c8facd5061209cbf465a91b9
Paul Nguyen <paul.tqh.nguyen@gmail.com>
2021-02-13 23:10:22 -0800

    save

--- static_autodiff/setup.py
+++ static_autodiff/setup.py
@@ -1,4 +1,135 @@
+
 from setuptools import setup
+import distutils.cmd
+import distutils.log
+
+import os
+import subprocess
+import shutil
+
+from typing import Tuple
+
+TIBS_COMPILER_PATH = os.path.abspath('./tibs/compiler/')
+
+LLVM_PROJECT_GIT_URL = 'https://github.com/llvm/llvm-project.git'
+
+LLVM_PROJECT_GIT_COMMIT_HASH = '805d59593f50a5c5050c0fc5cb9fbe02cd751511'
+
+LLVM_PROJECT_REPO_LOCATION = os.path.abspath(os.path.join(TIBS_COMPILER_PATH, 'llvm-project'))
+
+LLVM_REPO_BUILD_DIR = os.path.join(LLVM_PROJECT_REPO_LOCATION, 'build')
+
+LLVM_REPO_BIN_DIR = os.path.join(LLVM_REPO_BUILD_DIR, 'bin')
+
+TIBS_COMPILER_BUILD_DIR = os.path.join(TIBS_COMPILER_PATH, 'mlir/build')
+
+def run_shell_commands(directory: str, *commands: Tuple[str], **environment_variables) -> None:
+    command = ' && '.join(
+        [f'pushd {directory}'] +
+        [f'export {name}={value}' for name, value in environment_variables.items()] + 
+        list(commands) +
+        ['popd']
+    )
+    
+    process = subprocess.Popen('/bin/bash', stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    stdout_string, stderr_string = process.communicate(command.encode("utf-8"))
+    stdout_string = stdout_string.decode("utf-8")
+    stderr_string = stderr_string.decode("utf-8")
+    return_code = process.returncode
+    
+    if return_code != 0:
+        error_string = (
+            f'Command Failed wit exit code {return_code}:' + '\n' +
+            command + '\n' +
+            'STDOUT Messages:' + '\n' +
+            stdout_string + '\n' +
+            'STDERR Messages:' + '\n' +
+            stderr_string
+        )
+        raise RuntimeError(error_string)
+    return
+
+class CompileTibsCompilerCommand(distutils.cmd.Command):
+    
+    description = 'Compile the TIBS compiler.'
+    user_options = [
+        ('compile-clean=', None, 'Compile from a clean state.'),
+    ]
+
+    def initialize_options(self) -> None:
+        self.compile_clean = 'false'
+        return
+
+    def finalize_options(self) -> None:
+        self.compile_clean = False if self.compile_clean.lower().strip() == 'false' else True
+        return
+
+    def run(self) -> None:
+        if 'CONDA_PREFIX' not in os.environ:
+            raise NotImplementedError(f'Compilation of TIBS compiler not currently supported outside of a conda environment.')
+        self.clone_local_llvm()
+        self.compile_local_llvm()
+        self.compile_tibs_compiler()
+        self.announce(f'Finished compiling TIBS compiler.', level=distutils.log.INFO)
+        return
+    
+    def clone_local_llvm(self) -> None:
+        if self.compile_clean:
+            shutil.rmtree(LLVM_PROJECT_REPO_LOCATION)
+        if not os.path.isdir(LLVM_PROJECT_REPO_LOCATION):
+            self.announce(f'Cloning local LLVM repo into {LLVM_PROJECT_REPO_LOCATION}.', level=distutils.log.INFO)
+            run_shell_commands(TIBS_COMPILER_PATH, f'git clone {LLVM_PROJECT_GIT_URL}')
+            
+            self.announce(f'Resetting local LLVM repo at {LLVM_PROJECT_REPO_LOCATION} to commit {LLVM_PROJECT_GIT_COMMIT_HASH}.', level=distutils.log.INFO)
+            run_shell_commands(LLVM_PROJECT_REPO_LOCATION, f'git reset --hard  {LLVM_PROJECT_GIT_COMMIT_HASH}')
+        return
+    
+    def compile_local_llvm(self) -> None:
+        self.announce(f'Compiling local LLVM repo at {LLVM_PROJECT_REPO_LOCATION}.', level=distutils.log.INFO)
+        
+        if not os.path.isdir(LLVM_REPO_BUILD_DIR):
+            os.makedirs(LLVM_REPO_BUILD_DIR)
+        
+        if self.compile_clean or not os.path.isdir(LLVM_REPO_BIN_DIR):
+            PATH = os.environ['PATH']
+            LD_LIBRARY_PATH = os.environ['LD_LIBRARY_PATH']
+
+            if LLVM_REPO_BIN_DIR not in PATH.split(':'):
+                PATH = PATH + ':' + LLVM_REPO_BIN_DIR
+            
+            conda_prefix = os.environ['CONDA_PREFIX']
+            conda_lib_dir = os.path.join(conda_prefix, 'lib')
+            if conda_lib_dir not in LD_LIBRARY_PATH.split(':'):
+                LD_LIBRARY_PATH = LD_LIBRARY_PATH + ':' + conda_lib_dir
+                
+            run_shell_commands(
+                LLVM_REPO_BUILD_DIR,
+                f'cmake -G Ninja ../llvm -DLLVM_ENABLE_PROJECTS=mlir -DLLVM_BUILD_EXAMPLES=ON -DLLVM_TARGETS_TO_BUILD="X86;NVPTX;AMDGPU" -DCMAKE_BUILD_TYPE=Release -DLLVM_ENABLE_ASSERTIONS=ON',
+                f'cmake --build . --target check-mlir',
+                PATH=PATH,
+                LD_LIBRARY_PATH=LD_LIBRARY_PATH,
+            )
+        
+        assert os.path.isdir(LLVM_REPO_BIN_DIR)
+        
+        return
+
+    def compile_tibs_compiler(self) -> None:
+        self.announce(f'Compiling the TIBS compiler.', level=distutils.log.INFO)
+        
+        if not os.path.isdir(TIBS_COMPILER_BUILD_DIR):
+            os.makedirs(TIBS_COMPILER_BUILD_DIR)
+        
+        run_shell_commands(
+            TIBS_COMPILER_BUILD_DIR,
+            'cmake -G Ninja .. -DMLIR_DIR=$PREFIX/lib/cmake/mlir -DLLVM_EXTERNAL_LIT=$BUILD_DIR/bin/llvm-lit',
+            'ninja',
+            BUILD_DIR=LLVM_REPO_BUILD_DIR,
+            PREFIX=LLVM_REPO_BUILD_DIR
+        )
+
+        return
+        
 
 setup( # TODO add more here
     name="TIBS",
@@ -6,4 +137,8 @@ setup( # TODO add more here
     author="Paul Nguyen",
     packages=["tibs"],
     python_requires=">=3.7",
+    cmdclass={
+        'compile_tibs_compiler': CompileTibsCompilerCommand,
+    },
+
 )
--- static_autodiff/tibs/compiler/__init__.py
+++ static_autodiff/tibs/compiler/__init__.py
@@ -22,136 +22,9 @@ from ..misc_utilities import *
 
 CURRENT_MODULE_PATH = os.path.dirname(os.path.realpath(__file__))
 
-LLVM_PROJECT_GIT_URL = 'https://github.com/llvm/llvm-project.git'
+LIBTIBS_SO_LOCATION = os.path.abspath(os.path.join(CURRENT_MODULE_PATH, 'mlir/build/tibs-compiler/libtibs-compiler.so'))
 
-LLVM_PROJECT_GIT_COMMIT_HASH = '805d59593f50a5c5050c0fc5cb9fbe02cd751511'
-
-LLVM_PROJECT_REPO_LOCATION = os.path.abspath(os.path.join(CURRENT_MODULE_PATH, 'llvm-project'))
-
-@trace
-def clone_local_llvm() -> None:
-    # TODO use the MLILR conda-forge package https://anaconda.org/conda-forge/mlir
-    if not os.path.isdir(LLVM_PROJECT_REPO_LOCATION):
-        print(f'Cloning local LLVM repo into {LLVM_PROJECT_REPO_LOCATION}.')
-        command = ' && '.join([
-            f'pushd {CURRENT_MODULE_PATH}', 
-            f'git clone {LLVM_PROJECT_GIT_URL}', 
-            'popd', 
-        ])
-        print('LLVM Repo Clone Command:')
-        print(command)
-        clone_stdout, clone_stderr, clone_return_code= shell(command)
-        print('LLVM Repo Clone Output:')
-        print(clone_stdout)
-        print('LLVM Repo Clone Error Messages:')
-        print(clone_stderr)
-        if clone_return_code != 0:
-            raise RuntimeError(f'Could not clone LLVM.')
-        
-        print(f'Resetting local LLVM repo at {LLVM_PROJECT_REPO_LOCATION} to commit {LLVM_PROJECT_GIT_COMMIT_HASH}.')
-        command = ' && '.join([
-            f'pushd {LLVM_PROJECT_REPO_LOCATION}', 
-            f'git reset --hard  {LLVM_PROJECT_GIT_COMMIT_HASH}',
-            'popd'
-        ])
-        print('LLVM Repo Reset Command:')
-        print(command)
-        reset_stdout, reset_stderr, reset_return_code = shell(command)
-        print('LLVM Repo Reset Output:')
-        print(reset_stdout)
-        print('LLVM Repo Reset Error Messages:')
-        print(reset_stderr)
-        if reset_return_code != 0:
-            raise RuntimeError(f'Could not reset local LLVM repo to correct commit at {LLVM_PROJECT_GIT_COMMIT_HASH}. Try deleting {LLVM_PROJECT_REPO_LOCATION}.')
-    return
-
-@trace # TODO move this to be within compile_local_llvm
-def set_environment_variables() -> None:
-    # Verify PATH environment variable
-    llvm_repo_build_dir = os.path.join(LLVM_PROJECT_REPO_LOCATION, "build")
-    llvm_repo_bin_dir = os.path.join(llvm_repo_build_dir, 'bin')
-    if llvm_repo_bin_dir not in os.environ['PATH'].split(':'):
-        os.environ['PATH'] = os.environ['PATH'] + ':' + llvm_repo_bin_dir
-    # Verify LD_LIBRARY_PATH environment variable
-    if 'CONDA_PREFIX' not in os.environ:
-        raise NotImplementedError(f'Compilation not currently supported outside of a conda environment.')
-    conda_prefix = os.environ['CONDA_PREFIX']
-    conda_lib_dir = os.path.join(conda_prefix, 'lib')
-    if conda_lib_dir not in os.environ['LD_LIBRARY_PATH'].split(':'):
-        os.environ['LD_LIBRARY_PATH'] = os.environ['LD_LIBRARY_PATH'] + ':' + conda_lib_dir
-    return
-
-@trace
-def compile_local_llvm() -> None:
-    print(f'Compiling local LLVM repo at {LLVM_PROJECT_REPO_LOCATION}.')
-    llvm_repo_build_dir = os.path.join(LLVM_PROJECT_REPO_LOCATION, 'build')
-    llvm_repo_bin_dir = os.path.join(llvm_repo_build_dir, 'bin')
-    if not os.path.isdir(llvm_repo_build_dir):
-        os.makedirs(llvm_repo_build_dir)
-    print(f"os.environ['PATH'] {repr(os.environ['PATH'])}") # TODO Remove this
-    print(f"os.environ['LD_LIBRARY_PATH'] {repr(os.environ['LD_LIBRARY_PATH'])}") # TODO Remove this
-    command = ' && '.join([
-        f'pushd {llvm_repo_build_dir}',
-        f'cmake -G Ninja ../llvm -DLLVM_ENABLE_PROJECTS=mlir -DLLVM_BUILD_EXAMPLES=ON -DLLVM_TARGETS_TO_BUILD="X86;NVPTX;AMDGPU" -DCMAKE_BUILD_TYPE=Release -DLLVM_ENABLE_ASSERTIONS=ON',
-        f'cmake --build . --target check-mlir',
-        f'popd',
-    ])
-    print('LLVM Repo Compile Command:')
-    print(command)
-    llvm_compile_stdout, llvm_compile_stderr, return_code = shell(command)
-    print('LLVM Repo Compile Output:')
-    print(llvm_compile_stdout)
-    print('LLVM Repo Compile Error Messages:')
-    print(llvm_compile_stderr)
-    if return_code != 0:
-        raise RuntimeError(f'Could not compile local LLVM repo.')
-    assert os.path.isdir(llvm_repo_bin_dir)
-    return
-
-@trace
-def compile_tibs_compiler() -> None:
-    print(f'Compiling the TIBS compiler.')
-    
-    # TODO move these environment variables into local pyton variables
-    # Verify BUILD_DIR and PREFIX environment variables
-    llvm_repo_build_dir = os.path.join(LLVM_PROJECT_REPO_LOCATION, 'build')
-    os.environ['BUILD_DIR'] = llvm_repo_build_dir
-    os.environ['PREFIX'] = llvm_repo_build_dir
-    
-    build_dir = os.path.join(CURRENT_MODULE_PATH, 'mlir/build')
-    if not os.path.isdir(build_dir):
-        os.makedirs(build_dir)
-    print(f"os.environ['PATH'] {repr(os.environ['PATH'])}") # TODO Remove this
-    print(f"os.environ['LD_LIBRARY_PATH'] {repr(os.environ['LD_LIBRARY_PATH'])}") # TODO Remove this
-    print(f"os.environ['BUILD_DIR'] {repr(os.environ['BUILD_DIR'])}") # TODO Remove this
-    print(f"os.environ['PREFIX'] {repr(os.environ['PREFIX'])}") # TODO Remove this
-    command = ' && '.join([
-        'echo PATH $PATH', # TODO Remove this
-        'echo LD_LIBRARY_PATH $LD_LIBRARY_PATH', # TODO Remove this
-        'echo BUILD_DIR $BUILD_DIR', # TODO Remove this
-        'echo PREFIX $PREFIX', # TODO Remove this
-        f'pushd {build_dir}',
-        'cmake -G Ninja .. -DMLIR_DIR=$PREFIX/lib/cmake/mlir -DLLVM_EXTERNAL_LIT=$BUILD_DIR/bin/llvm-lit',
-        'ninja',
-        'popd'
-    ])
-    print('Compilation of TIBS Compiler Command:')
-    print(command)
-    compile_stdout, compile_stderr, return_code = shell(command)
-    print('Compilation of TIBS Compiler Output:')
-    print(compile_stdout)
-    print('Compilation of TIBS Compiler Error Messages:')
-    print(compile_stderr)
-    if return_code != 0:
-        raise RuntimeError(f'Could not compile TIBS compiler.')
-    return 
-
-clone_local_llvm()
-set_environment_variables()
-compile_local_llvm()
-compile_tibs_compiler()
-
-LIBTIBS_SO_LOCATION = os.path.abspath(os.path.join(CURRENT_MODULE_PATH, 'mlir/build/tibs-compiler/libtibs-compiler.so')) # TODO update this
+assert os.path.isdir(LIBTIBS_SO_LOCATION), 'The Tibs compiler has not yet been compiled.'
 
 LIBTIBS_SO = ctypes.CDLL(LIBTIBS_SO_LOCATION)
 
@@ -180,4 +53,4 @@ def compile():
 ##########
 
 if __name__ == '__main__':
-    print("TODO add something here")
+    print('TODO add something here')
