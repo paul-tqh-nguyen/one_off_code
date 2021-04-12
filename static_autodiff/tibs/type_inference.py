7c2ff616f7c6583e056a21869f7a59b5a108182a
Paul Nguyen <paul.tqh.nguyen@gmail.com>
2021-04-12 00:26:35 -0700

    save

--- static_autodiff/tibs/type_inference.py
+++ static_autodiff/tibs/type_inference.py
@@ -275,6 +275,7 @@ def function_definition_type_inference(ast_node: FunctionDefinitionASTNode, var_
     ast_node.function_signature
     var_name_to_type_info[ast_node.function_name] = ast_node
     function_body_var_name_to_type_info = dict(var_name_to_type_info)
+    redefined_variablles = set()
     for variable, variable_type in ast_node.function_signature:
         function_body_var_name_to_type_info[variable.name] = variable_type
     var_name_to_type_info, changed = perform_type_inference(ast_node.function_body, var_name_to_type_info, ast_node.function_name)
