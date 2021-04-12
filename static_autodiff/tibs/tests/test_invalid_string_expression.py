5d75c533e0df795c86f83b7cbfcf3751d36a7036
Paul Nguyen <paul.tqh.nguyen@gmail.com>
2021-04-11 19:24:50 -0700

    save

---
+++ static_autodiff/tibs/tests/test_invalid_string_expression.py
@@ -0,0 +1,18 @@
+
+import pytest
+from tibs import parser, type_inference
+from tibs.misc_utilities import *
+
+INVALID_STRING_EXPRESSION = [pytest.param(*args, id=f'invalid_conditional_{i}') for i, args in enumerate([
+    (
+        ' "a" << 1.2 ',
+        parser.ParseError,
+        'Could not parse the following'
+    ),
+])]
+
+@pytest.mark.parametrize('input_string, exception_type, error_match_string', INVALID_STRING_EXPRESSION)
+def test_invalid_string_expression(input_string, exception_type, error_match_string):
+    with pytest.raises(exception_type, match=error_match_string):
+        result = parser.parseSourceCode(input_string)
+        type_inference.perform_type_inference(result)
