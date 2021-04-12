55657b9ec4efffbc64fa2ead55c8e3969c167fec
Paul Nguyen <paul.tqh.nguyen@gmail.com>
2021-04-11 23:13:07 -0700

    save

--- static_autodiff/tibs/tests/test_invalid_module.py
+++ static_autodiff/tibs/tests/test_invalid_module.py
@@ -3,7 +3,7 @@ import pytest
 from tibs import parser, type_inference
 from tibs.misc_utilities import *
 
-INVALID_MODULE_CASES = [pytest.param(*args, id=f'invalid_module_{i}') for i, args in enumerate([
+INVALID_MODULE_CASES = tuple(pytest.param(*args, id=f'invalid_module_{i}') for i, args in enumerate([
     (
         'function f(a: Integer) -> NothingType return ; f(a:=True or False)',
         type_inference.TypeInferenceConsistencyError,
@@ -19,7 +19,7 @@ INVALID_MODULE_CASES = [pytest.param(*args, id=f'invalid_module_{i}') for i, arg
         type_inference.TypeInferenceConsistencyError,
         'asdfadsfre'
     ),
-])]
+]))
 
 @pytest.mark.parametrize('input_string, exception_type, error_match_string', INVALID_MODULE_CASES)
 def test_invalid_module(input_string, exception_type, error_match_string):
