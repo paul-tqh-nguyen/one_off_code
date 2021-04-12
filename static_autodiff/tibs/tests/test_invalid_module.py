e697cd582fb82ec4a0eeeea01282c77a70662c07
Paul Nguyen <paul.tqh.nguyen@gmail.com>
2021-04-11 23:41:15 -0700

    save

--- static_autodiff/tibs/tests/test_invalid_module.py
+++ static_autodiff/tibs/tests/test_invalid_module.py
@@ -27,7 +27,7 @@ INVALID_MODULE_CASES = tuple(pytest.param(*args, id=f'invalid_module_{i}') for i
     (
         'function f(x: Integer, x: Integer) -> NothingType return',
         Exception,
-        r'12ewdf'
+        r'has redundantly defined parameters'
     ),
     (
         'function f(x: Integer, y: Integer) -> NothingType return ; f(x:=2, y:=1, y:=1)',
