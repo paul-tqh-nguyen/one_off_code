dab37289916f073e2742f6e190216bce8f0de3c0
Paul Nguyen <paul.tqh.nguyen@gmail.com>
2021-04-11 23:25:37 -0700

    save

--- static_autodiff/tibs/ast_node.py
+++ static_autodiff/tibs/ast_node.py
@@ -409,7 +409,7 @@ class FunctionCallExpressionASTNode(ExpressionASTNode):
         assert len(tokens) is 2
         function_name = tokens[0]
         arg_bindings = eager_map(tuple, map(pyparsing.ParseResults.asList, tokens[1]))
-        ASSERT.ParseError(len({variable_name for variable_name, _ in arg_bindings}) == len(arg_bindings), f'{function_name} has redundantly defined parameters.')
+        ASSERT.ParseError(len({variable.name for variable, _ in arg_bindings}) == len(arg_bindings), f'{function_name} has redundantly defined parameters.')
         node_instance = cls(function_name, arg_bindings)
         return node_instance
 
