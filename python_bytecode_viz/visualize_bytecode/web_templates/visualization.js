a6b8435354b65bbb476481b14d33e6554bca3730
Paul Nguyen <paul.tqh.nguyen@gmail.com>
2021-10-11 18:09:56 -0500

    viz

--- python_bytecode_viz/visualize_bytecode/web_templates/visualization.js
+++ python_bytecode_viz/visualize_bytecode/web_templates/visualization.js
@@ -33,6 +33,15 @@ const createRainbowColormap = (shadeCount) => {
     return colors;
 };
 
+// D3 Extensions
+d3.selection.prototype.moveToFront = function() {
+    return this.each(function() {
+        if (this.parentNode !== null) {
+            this.parentNode.appendChild(this);
+        }
+    });
+};
+
 const visualizationMain = () => {
     const dataLocation = './bytecode.json';
 
@@ -53,7 +62,7 @@ const visualizationMain = () => {
                     console.error("Cannot yet handle basic blocks without corresponding source code lines.");
                 }
             });
-            
+
             const codeDisplayElement = document.getElementById('code-table');
             let sourceCodeLineNumber = data.source_code_line_number;
             let previousBasicBlockId = -1;
@@ -63,18 +72,22 @@ const visualizationMain = () => {
             const colorMap = createRainbowColormap(data.nodes.length+1);
             data.source_code_lines.forEach(sourceLine => {
                 const rowElement = document.createElement('tr');
-                
+    
                 const sourceTdElement = document.createElement('td');
+                const sourceDivElement = document.createElement('div');
                 const sourcePreElement = document.createElement('pre');
                 sourcePreElement.innerHTML =
                     String(sourceCodeLineNumber).padStart(lineNumberWidth, '0')
                     + ' '
                     + sourceLine.slice(numLeadingSpaces);
-                sourceTdElement.append(sourcePreElement);
+                sourceDivElement.append(sourcePreElement);
+                sourceTdElement.append(sourceDivElement);
                 rowElement.append(sourceTdElement);
-                
+    
                 let colorIndex = data.nodes.length+1;
                 const bytecodeTdElement = document.createElement('td');
+                const bytecodeDivElement = document.createElement('div');
+                bytecodeTdElement.append(bytecodeDivElement);
                 if (sourceCodeLineNumber in sourceCodeLineNumberToBasicBlockId) {
                     const basicBlockId = sourceCodeLineNumberToBasicBlockId[sourceCodeLineNumber];
                     colorIndex = basicBlockIdToBasicBlockDict[basicBlockId].sequentialIndex;
@@ -86,7 +99,7 @@ const visualizationMain = () => {
                         basicBlockIdToBasicBlockDict[basicBlockId].pretty_strings.forEach(prettyString => {
                             const bytecodePreElement = document.createElement('pre');
                             bytecodePreElement.innerHTML = prettyString;
-                            bytecodeTdElement.append(bytecodePreElement);
+                            bytecodeDivElement.append(bytecodePreElement);
                         });
                         bytecodeTdElement.setAttribute('rowspan', 1);
                         rowElement.append(bytecodeTdElement);
@@ -97,14 +110,90 @@ const visualizationMain = () => {
                     previousBytecodeTdElement.setAttribute('rowspan', newRowSpanValue);
                 }
                 codeDisplayElement.append(rowElement);
-                
+    
                 const rowColor = colorMap[colorIndex];
                 sourceTdElement.style.borderColor = rowColor;
+                // FIXME sometimes this is useless when we don't use the td element
                 bytecodeTdElement.style.borderColor = rowColor;
-                
+    
                 sourceCodeLineNumber++;
             });
-            
+
+            const plotContainer = document.getElementById('cfg-container');
+            const svg = d3.select('#cfg-svg');
+
+            const render = () => {
+
+                const basicBlockTextBoundingBoxPadding = 15;
+    
+                svg
+	            .attr('width', `0px`)
+	            .attr('height', `0px`)
+	            .attr('width', `${plotContainer.clientWidth}px`)
+	            .attr('height', `${plotContainer.clientHeight}px`);
+    
+                const basicBlockGroup = svg.append('g');
+
+                const basicBlockDataJoin = basicBlockGroup
+                      .selectAll('text')
+                      .data(data.nodes);
+
+                const basicBlockTextEnterSelection = basicBlockDataJoin
+                      .enter()
+                      .append('text')
+                      .attr('id', datum => `basic-block-${datum.id}`)
+                      .attr('class', 'basic-block')
+                      .attr('x', datum => {
+                          return 10*(2+datum.id);
+                      })
+                      .attr('y', datum => {
+                          return 10*(2+datum.id);
+                      })
+                      .html(datum => {
+                          const textElementId = `basic-block-${datum.id}`;
+                          const textElementBBox = svg.select(`#${textElementId}`).node().getBBox();
+                          return datum.pretty_strings.map(
+                              string => `<tspan x=${textElementBBox.x} dx=0 dy=22>${string}</tspan>`
+                          ).join('\n');
+                      });
+
+                const  basicBlockBoundingBoxEnterSelection = basicBlockDataJoin
+                      .enter()
+                      .append('rect')
+                      .attr('id', datum => `basic-block-bounding-box-{datum.id}`)
+                      .attr('stroke', datum => colorMap[datum.sequentialIndex])
+                      .attr('class', 'basic-block-bounding-box');
+
+                basicBlockBoundingBoxEnterSelection
+                    .attr('x', datum => {
+                        const textElementId = `basic-block-${datum.id}`;
+                        const textElementBBox = svg.select(`#${textElementId}`).node().getBBox();
+                        const x = textElementBBox.x - basicBlockTextBoundingBoxPadding;
+                        return x;
+                    })
+                    .attr('y', datum => {
+                        const textElementId = `basic-block-${datum.id}`;
+                        const textElementBBox = svg.select(`#${textElementId}`).node().getBBox();
+                        const y = textElementBBox.y - basicBlockTextBoundingBoxPadding;
+                        return y;
+                    })
+                    .attr('width', datum => {
+                        const textElementId = `basic-block-${datum.id}`;
+                        const textElementBBox = svg.select(`#${textElementId}`).node().getBBox();
+                        return textElementBBox.width + 2 * basicBlockTextBoundingBoxPadding;
+                    })
+                    .attr('height', datum => {
+                        const textElementId = `basic-block-${datum.id}`;;
+                        const textElementBBox = svg.select(`#${textElementId}`).node().getBBox();
+                        return textElementBBox.height + 2 * basicBlockTextBoundingBoxPadding;
+                    });
+
+                basicBlockTextEnterSelection.moveToFront();
+            };
+            render();
+            window.addEventListener('resize', render);
+
+
         }).catch(err => {
             console.error(err.message);
             return;
